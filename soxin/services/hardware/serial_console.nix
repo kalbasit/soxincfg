b21205f309643d280fc7f1ca09f8b12d47f7c240
Wael Nasreddine <wael.nasreddine@gmail.com>
2020-12-21 10:42:34 -0800

    soxin/hardware: introducte a module for serial consoles

---
+++ soxin/services/hardware/serial_console.nix
@@ -0,0 +1,18 @@
+{ config, lib, mode, ... }:
+
+with lib;
+
+{
+  options.soxin.hardware.serial_console.enable = mkEnableOption "Enable serial console";
+
+  config = mkIf config.soxin.hardware.serial_console.enable (mkMerge [
+    (optionalAttrs (mode == "NixOS") {
+      boot.kernelParams = [ "console=tty0 console=ttyS0,115200n8" ];
+      boot.loader.grub.extraConfig = ''
+        serial --speed=115200 --unit=0 --word=8 --parity=no --stop=1
+        terminal_input serial console
+        terminal_output serial console
+      '';
+    })
+  ]);
+}
