60a74de1e4ed0434834788ec730e7962e1ca95c8
Wael Nasreddine <wael.nasreddine@gmail.com>
2020-12-21 10:33:04 -0800

    profiles/workstation: remove redundant and unused packages

--- profiles/workstation/default.nix
+++ profiles/workstation/default.nix
@@ -142,96 +142,96 @@ mkMerge [
     services.flameshot.enable = true;
 
     home.file = {
-        ".config/greenclip.cfg".text = ''
-          Config {
-           maxHistoryLength = 250,
-           historyPath = "~/.cache/greenclip.history",
-           staticHistoryPath = "~/.cache/greenclip.staticHistory",
-           imageCachePath = "/tmp/",
-           usePrimarySelectionAsInput = False,
-           blacklistedApps = [],
-           trimSpaceFromSelection = True
-          }
-        '';
+      ".config/greenclip.cfg".text = ''
+        Config {
+         maxHistoryLength = 250,
+         historyPath = "~/.cache/greenclip.history",
+         staticHistoryPath = "~/.cache/greenclip.staticHistory",
+         imageCachePath = "/tmp/",
+         usePrimarySelectionAsInput = False,
+         blacklistedApps = [],
+         trimSpaceFromSelection = True
+        }
+      '';
 
-        ".npmrc".text = "prefix=${config.home.homeDirectory}/.filesystem";
+      ".npmrc".text = "prefix=${config.home.homeDirectory}/.filesystem";
 
-        ".gnupg/gpg.conf".text = ''
-          # NOTE: https://github.com/jclement/dotfiles/blob/master/other/gpg.conf
-          #
-          # This is an implementation of the Riseup OpenPGP Best Practices
-          # https://help.riseup.net/en/security/message-security/openpgp/best-practices
-          #
+      ".gnupg/gpg.conf".text = ''
+        # NOTE: https://github.com/jclement/dotfiles/blob/master/other/gpg.conf
+        #
+        # This is an implementation of the Riseup OpenPGP Best Practices
+        # https://help.riseup.net/en/security/message-security/openpgp/best-practices
+        #
 
-          #-----------------------------
-          # default key
-          #-----------------------------
+        #-----------------------------
+        # default key
+        #-----------------------------
 
-          # The default key to sign with. If this option is not used, the default key is
-          # the first key found in the secret keyring
+        # The default key to sign with. If this option is not used, the default key is
+        # the first key found in the secret keyring
 
-          default-key 0x7ED8B6DE75BADCF9
+        default-key 0x7ED8B6DE75BADCF9
 
-          #-----------------------------
-          # behavior
-          #-----------------------------
+        #-----------------------------
+        # behavior
+        #-----------------------------
 
-          # Disable inclusion of the version string in ASCII armored output
-          no-emit-version
+        # Disable inclusion of the version string in ASCII armored output
+        no-emit-version
 
-          # Disable comment string in clear text signatures and ASCII armored messages
-          no-comments
+        # Disable comment string in clear text signatures and ASCII armored messages
+        no-comments
 
-          # Disable auto-starting the agent. On hosts that I'm using the GnuPG, I'd like
-          # to have home-manager start the agent on login.
-          no-autostart
+        # Disable auto-starting the agent. On hosts that I'm using the GnuPG, I'd like
+        # to have home-manager start the agent on login.
+        no-autostart
 
-          # Display long key IDs
-          keyid-format 0xlong
+        # Display long key IDs
+        keyid-format 0xlong
 
-          # List all keys (or the specified ones) along with their fingerprints
-          with-fingerprint
+        # List all keys (or the specified ones) along with their fingerprints
+        with-fingerprint
 
-          # Display the calculated validity of user IDs during key listings
-          list-options show-uid-validity
-          verify-options show-uid-validity
+        # Display the calculated validity of user IDs during key listings
+        list-options show-uid-validity
+        verify-options show-uid-validity
 
-          #-----------------------------
-          # keyserver
-          #-----------------------------
+        #-----------------------------
+        # keyserver
+        #-----------------------------
 
-          # This is the server that --recv-keys, --send-keys, and --search-keys will
-          # communicate with to receive keys from, send keys to, and search for keys on
-          keyserver hkps://hkps.pool.sks-keyservers.net
+        # This is the server that --recv-keys, --send-keys, and --search-keys will
+        # communicate with to receive keys from, send keys to, and search for keys on
+        keyserver hkps://hkps.pool.sks-keyservers.net
 
-          # When using --refresh-keys, if the key in question has a preferred keyserver
-          # URL, then disable use of that preferred keyserver to refresh the key from
-          keyserver-options no-honor-keyserver-url
+        # When using --refresh-keys, if the key in question has a preferred keyserver
+        # URL, then disable use of that preferred keyserver to refresh the key from
+        keyserver-options no-honor-keyserver-url
 
-          # When searching for a key with --search-keys, include keys that are marked on
-          # the keyserver as revoked
-          keyserver-options include-revoked
+        # When searching for a key with --search-keys, include keys that are marked on
+        # the keyserver as revoked
+        keyserver-options include-revoked
 
-          #-----------------------------
-          # algorithm and ciphers
-          #-----------------------------
+        #-----------------------------
+        # algorithm and ciphers
+        #-----------------------------
 
-          # list of personal digest preferences. When multiple digests are supported by
-          # all recipients, choose the strongest one
-          personal-cipher-preferences AES256 AES192 AES CAST5
+        # list of personal digest preferences. When multiple digests are supported by
+        # all recipients, choose the strongest one
+        personal-cipher-preferences AES256 AES192 AES CAST5
 
-          # list of personal digest preferences. When multiple ciphers are supported by
-          # all recipients, choose the strongest one
-          personal-digest-preferences SHA512 SHA384 SHA256 SHA224
+        # list of personal digest preferences. When multiple ciphers are supported by
+        # all recipients, choose the strongest one
+        personal-digest-preferences SHA512 SHA384 SHA256 SHA224
 
-          # message digest algorithm used when signing a key
-          cert-digest-algo SHA512
+        # message digest algorithm used when signing a key
+        cert-digest-algo SHA512
 
-          # This preference list is used for new keys and becomes the default for
-          # "setpref" in the edit menu
-          default-preference-list SHA512 SHA384 SHA256 SHA224 AES256 AES192 AES CAST5 ZLIB BZIP2 ZIP Uncompressed
-        '';
-      };
+        # This preference list is used for new keys and becomes the default for
+        # "setpref" in the edit menu
+        default-preference-list SHA512 SHA384 SHA256 SHA224 AES256 AES192 AES CAST5 ZLIB BZIP2 ZIP Uncompressed
+      '';
+    };
 
     home.packages = with pkgs; [
       remmina
@@ -245,8 +245,6 @@ mkMerge [
 
       amazon-ecr-credential-helper
       docker-credential-gcr
-
-      browsh
 
       gist
 
@@ -277,7 +275,6 @@ mkMerge [
 
       nur.repos.kalbasit.swm
 
-      corgi
       vgo2nix
 
       unzip
@@ -292,8 +289,6 @@ mkMerge [
       gotop
 
       jetbrains.idea-community
-
-      keybase
 
       slack
 
