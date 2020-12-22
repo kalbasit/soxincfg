51e379a45ecea93d791063aebed3fb10dc1fc091
Wael Nasreddine <wael.nasreddine@gmail.com>
2020-12-21 16:59:58 -0800

    programs.starship: fix the order of battery.display

--- modules/programs/starship.nix
+++ modules/programs/starship.nix
@@ -18,11 +18,11 @@ in
       programs.starship = {
         settings = {
           battery.display = [
-            { threshold = 100; style = "bold green"; }
-            { threshold = 30; style = "yellow"; }
-            { threshold = 20; style = "bold yellow"; }
-            { threshold = 15; style = "red"; }
             { threshold = 10; style = "bold red"; }
+            { threshold = 15; style = "red"; }
+            { threshold = 20; style = "bold yellow"; }
+            { threshold = 30; style = "yellow"; }
+            { threshold = 100; style = "bold green"; }
           ];
           env_var = {
             variable = "ZSH_PROFILE";
