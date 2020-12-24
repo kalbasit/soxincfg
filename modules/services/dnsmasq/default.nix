73e4be504c63b6575da3f065d4adf09b8db51364
Wael Nasreddine <wael.nasreddine@gmail.com>
2020-12-24 13:27:14 -0800

    hosts/kore: install a DNSMasq server that block Ads (#8)
    The DNSMasq server will use https://github.com/notracking/hosts-blocklists
    to block ads and trackers. I've also added a systemd timer that updates
    the hosts-blocklists once a day at 11PM which should be an hour after it
    got updated upstream.

---
+++ modules/services/dnsmasq/default.nix
@@ -0,0 +1,65 @@
+{ config, lib, mode, options, pkgs, ... }:
+
+with lib;
+let
+  cfg = config.soxincfg.services.dnsmasq;
+in
+{
+  options.soxincfg.services.dnsmasq = {
+    enable = mkEnableOption "Install and configure DNSMasq";
+
+    blockAds = mkEnableOption "Automatically pull the blocklist to block all ads";
+  };
+
+  config = mkIf cfg.enable (mkMerge [
+    (optionalAttrs (mode == "NixOS") {
+      services.dnsmasq = {
+        inherit (cfg) enable;
+
+        alwaysKeepRunning = true; # do not allow the system to turn the service off
+      };
+    })
+
+    (optionalAttrs (mode == "NixOS") (mkIf cfg.blockAds
+      {
+        services.dnsmasq.extraConfig = ''
+          conf-file=/var/lib/dnsmasq/notracking/hosts-blocklists/domains.txt
+          addn-hosts=/var/lib/dnsmasq/notracking/hosts-blocklists/hostnames.txt
+        '';
+
+        systemd.services.dnsmasq.preStart = with pkgs; ''
+          if ! [[ -d /var/lib/dnsmasq/notracking/hosts-blocklists ]]; then
+            mkdir -p /var/lib/dnsmasq/notracking
+            ${git}/bin/git clone https://github.com/notracking/hosts-blocklists.git /var/lib/dnsmasq/notracking/hosts-blocklists
+            chown -R dnsmasq /var/lib/dnsmasq/notracking
+          fi
+        '';
+
+        systemd.services.update-notracking-hosts-blocklists = {
+          description = "Update the notracking hosts blocklists";
+          after = [ "dnsmasq.service" ];
+          serviceConfig = {
+            ExecStart = with pkgs; writeScript "update-notracking-hosts-blocklists.sh" ''
+              #!${runtimeShell}
+              set -euo pipefail
+              cd /var/lib/dnsmasq/notracking/hosts-blocklists
+              ${git}/bin/git config pull.ff only
+              ${git}/bin/git pull
+              chown -R dnsmasq /var/lib/dnsmasq/notracking
+              systemctl reload dnsmasq.service
+            '';
+          };
+        };
+
+          systemd.timers.update-notracking-hosts-blocklists = {
+            description = "Update the notracking hosts blocklists once a day";
+            wantedBy = [ "timers.target" ];
+            timerConfig = {
+              Unit = "update-notracking-hosts-blocklists.service";
+              OnCalendar = "*-*-* 23:00:00";
+              OnBootSec = "15min";
+            };
+          };
+      }))
+  ]);
+}
