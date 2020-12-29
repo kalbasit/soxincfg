e51651ef7ce18373501a8cfb7f75ab6230f48a4b
Wael Nasreddine <wael.nasreddine@gmail.com>
2020-12-29 12:44:29 -0800

    profiles/workstation: Setup WiFi connections on all workstations

--- profiles/workstation/default.nix
+++ profiles/workstation/default.nix
@@ -113,40 +113,50 @@ mkMerge [
     };
   }
 
-  (optionalAttrs (mode == "NixOS") {
-    environment.homeBinInPath = true;
+  (optionalAttrs (mode == "NixOS")
+    (
+      let
+        sopsFile = ./secrets.sops.yaml;
+      in
+      {
+        environment.homeBinInPath = true;
 
-    services.gnome3.gnome-keyring.enable = true;
+        services.gnome3.gnome-keyring.enable = true;
 
-    services.logind = {
-      lidSwitch = "hybrid-sleep";
-      lidSwitchDocked = "ignore";
-      lidSwitchExternalPower = "hybrid-sleep";
-      extraConfig = ''
-        HandlePowerKey=suspend
-      '';
-    };
+        services.logind = {
+          lidSwitch = "hybrid-sleep";
+          lidSwitchDocked = "ignore";
+          lidSwitchExternalPower = "hybrid-sleep";
+          extraConfig = ''
+            HandlePowerKey=suspend
+          '';
+        };
 
-    # Redshift
-    location.latitude = 34.42;
-    location.longitude = -122.11;
-    services.redshift = {
-      brightness.day = "1.0";
-      brightness.night = "0.6";
-      enable = true;
-      temperature.day = 5900;
-      temperature.night = 3700;
-    };
+        # Redshift
+        location.latitude = 34.42;
+        location.longitude = -122.11;
+        services.redshift = {
+          brightness.day = "1.0";
+          brightness.night = "0.6";
+          enable = true;
+          temperature.day = 5900;
+          temperature.night = 3700;
+        };
 
-    # TODO: fix this!
-    system.extraSystemBuilderCmds = ''ln -sfn /yl/.surfingkeys.js $out/.surfingkeys.js'';
+        # TODO: fix this!
+        system.extraSystemBuilderCmds = ''ln -sfn /yl/.surfingkeys.js $out/.surfingkeys.js'';
 
-    # L2TP VPN does not connect without the presence of this file!
-    # https://github.com/NixOS/nixpkgs/issues/64965
-    system.activationScripts.ipsec-secrets = ''
-      touch $out/etc/ipsec.secrets
-    '';
-  })
+        # L2TP VPN does not connect without the presence of this file!
+        # https://github.com/NixOS/nixpkgs/issues/64965
+        system.activationScripts.ipsec-secrets = ''
+          touch $out/etc/ipsec.secrets
+        '';
+
+        sops.secrets._etc_NetworkManager_system-connections_Nasreddine_nmconnection = { inherit sopsFile; path = "/etc/NetworkManager/system-connections/Nasreddine.nmconnection"; };
+        sops.secrets._etc_NetworkManager_system-connections_Nasreddine-ADMIN_nmconnection = { inherit sopsFile; path = "/etc/NetworkManager/system-connections/Nasreddine-ADMIN.nmconnection"; };
+        sops.secrets._etc_NetworkManager_system-connections_Ellipsis_Jetpack_4976_nmconnection = { inherit sopsFile; path = "/etc/NetworkManager/system-connections/Ellipsis_Jetpack_4976.nmconnection"; };
+      }
+    ))
 
   (optionalAttrs (mode == "home-manager") {
     programs.bat.enable = true;
