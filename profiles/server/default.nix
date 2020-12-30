04bbf4b1bcecefe38e5d37559dae549342ec751d
Wael Nasreddine <wael.nasreddine@gmail.com>
2020-12-29 16:02:36 -0800

    profiles/server: install neovim in the environment

--- profiles/server/default.nix
+++ profiles/server/default.nix
@@ -1,7 +1,10 @@
+{ pkgs, ... }:
+
 {
   # Enable the installation of my neovim
   # TODO: Need a minimal neovim with just my keybindings
   # soxin.programs.neovim.enable = true;
+  environment.systemPackages = with pkgs; [ neovim ];
 
   # Enable SSH server on all of my hosts
   soxin.services.openssh.enable = true;
