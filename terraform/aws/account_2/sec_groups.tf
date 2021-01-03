c84b729992ac8e376f3727c23664e197fc5f95f8
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2021-01-03 14:14:24 -0800

    terraform/aws/account_2: open port 2022 on aarch64-linux-0 (#12)

--- terraform/aws/account_2/sec_groups.tf
+++ terraform/aws/account_2/sec_groups.tf
@@ -19,6 +19,27 @@ resource "aws_security_group" "allow_ssh_from_anywhere" {
   }
 }
 
+resource "aws_security_group" "allow_eternal_terminal_from_anywhere" {
+  name        = "allow_eternal_terminal_from_anywhere"
+  description = "Allow Eternal Terminal inbound traffic"
+  vpc_id      = module.vpc_us_west_1.vpc_id
+
+  ingress {
+    description = "SSH from anywhere"
+    from_port   = 2022
+    to_port     = 2022
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
+  tags = {
+    Name      = "allow_eternal_terminal_from_anywhere"
+    Owner     = "wael"
+    Terraform = "true"
+  }
+}
+
 resource "aws_security_group" "allow_egress_to_anywhere" {
   name        = "allow_egress_to_anywhere"
   description = "Allow all outgoing traffic"
