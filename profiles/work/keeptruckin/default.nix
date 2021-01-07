98f2f060ee75aecf1b1e9054a9ae2ecad5e3cc33
Wael Nasreddine <wael.nasreddine@gmail.com>
2021-01-07 14:19:34 -0800

    profiles/work/keeptruckin: Bind Mod4+j to open the current story in Jira
    Anytime I create a new story, I have to either drag a window with me or
    type in the URL manually. This should help me to quickly open the Jira
    ticket in my browser of choice for work.

--- profiles/work/keeptruckin/default.nix
+++ profiles/work/keeptruckin/default.nix
@@ -1,4 +1,4 @@
-{ config, lib, mode, ... }:
+{ config, lib, mode, pkgs, ... }:
 
 with lib;
 
@@ -36,6 +36,30 @@ with lib;
       programs.ssh.extraConfig = ''
         Include ~/.ssh/config_include_work_keeptruckin
       '';
+      xsession.windowManager.i3.config.keybindings = {
+        "Mod4+j" =
+          let
+            openJira = with pkgs; writeShellScript "open-jira.sh" ''
+              set -euo pipefail
+
+              active_workspace="$(i3-msg -t get_workspaces 2>/dev/null | ${jq}/bin/jq -r '.[] | if .focused == true then .name else empty end')"
+              if ! echo  "$active_workspace" | grep -q '.*@.*'; then
+                ${libnotify}/bin/notify-send 'Open Jira' "No story is currently in progress"
+                exit 1
+              fi
+
+              profile="$(echo "$active_workspace" | cut -d@ -f1)"
+              if [[ "$profile" != "keeptruckin" ]]; then
+                ${libnotify}/bin/notify-send 'Open Jira' "The profile $profile is not supported, only keeptruckin is. Sorry!"
+                exit 1
+              fi
+
+              story="$(echo "$active_workspace" | cut -d@ -f2)"
+              rbrowser --profile "$profile" --browser "chromium" "https://k2labs.atlassian.net/browse/$story" --new-window
+            '';
+          in
+          "exec --no-startup-id ${openJira}";
+      };
     }))
   ];
 }
