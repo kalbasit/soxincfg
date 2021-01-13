3acba979cd2a07a8cafae3021e540dd02ecfb58c
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2021-01-12 23:18:58 -0800

    modules/programs/weechat: new module enabled on workstations
    Copied the configuration from [cole-h][cole-h], and updated the config
    to use read-only files.
    
    I've also provided an 'edit-weechat-config' to open weechat with the
    configuration copied to a temporary location, and the encrypted files
    decrypted to allow the user to edit the config. Once weechat exits, the
    configuration is then copied back, and the encrypted files re-encrypted.
    
    [cole-h]: https://github.com/cole-h/nixos-config/blob/92749c8d5fa807692298c20c3819df07774c235c/users/vin/modules/weechat/default.nix

---
+++ modules/programs/weechat/default.nix
@@ -0,0 +1,120 @@
+{ mode, config, pkgs, lib, ... }:
+
+with lib;
+let
+  cfg = config.soxincfg.programs.weechat;
+
+  weechat-home = with pkgs; stdenvNoCC.mkDerivation {
+    name = "weechat-home";
+    src = ./config;
+    unpackPhase = ":";
+
+    preferLocalBuild = true;
+    allowSubstitutes = false;
+
+    installPhase = ''
+      mkdir $out
+
+      ln -s ${config.home.homeDirectory}/.weechat/logs $out/logs
+      ln -s ${config.home.homeDirectory}/.weechat/weechat.log $out/weechat.log
+      ln -s ${config.home.homeDirectory}/.weechat/urlserver_list.txt $out/urlserver_list.txt
+
+      ${rsync}/bin/rsync --exclude '*.sops' -avz $src/ $out/
+    '';
+  };
+
+  owner = config.users.users.yl.name;
+in
+{
+  options = {
+    soxincfg.programs.weechat = {
+      enable = mkEnableOption "WeeChat IRC client";
+    };
+  };
+
+  config = mkIf cfg.enable (mkMerge [
+    (optionalAttrs (mode == "NixOS") {
+      sops.secrets."_config_weechat_certs_freenode.pem" = { inherit owner; format = "binary"; sopsFile = ./config/certs/freenode.pem.sops; };
+      sops.secrets."_config_weechat_irc.conf" = { inherit owner; format = "binary"; sopsFile = ./config/irc.conf.sops; };
+      sops.secrets."_config_weechat_sec.conf" = { inherit owner; format = "binary"; sopsFile = ./config/sec.conf.sops; };
+    })
+
+    (optionalAttrs (mode == "home-manager") {
+      programs.zsh.shellAliases. weechat = "tmux -L weechat attach -t weechat";
+
+      home.activation.weechat = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+        mkdir -p ${config.home.homeDirectory}/.weechat/logs
+        touch ${config.home.homeDirectory}/.weechat/weechat.log
+        touch ${config.home.homeDirectory}/.weechat/urlserver_list.txt
+
+        chmod 511 ${config.home.homeDirectory}/.weechat
+      '';
+
+      home.packages = singleton (
+        pkgs.writeShellScriptBin "edit-weechat-config" ''
+          set -euo pipefail
+
+          local_dir="$(${pkgs.git}/bin/git rev-parse --show-toplevel)/modules/programs/weechat/config"
+          if ! [[ -d "$local_dir" ]]; then
+            echo "Please run this script from within soxincfg"
+            exit 1
+          fi
+
+          temp_dir="$(mktemp -d)"
+          ${pkgs.rsync}/bin/rsync -auz "$local_dir/" "$temp_dir/"
+
+          cd "$temp_dir"
+          for i in $(find . -name '*.sops'); do
+            rm -f "''${i%%.sops}"
+            ${pkgs.sops}/bin/sops -d "$i" > "''${i%%.sops}"
+          done
+
+          export WEECHAT_HOME="$temp_dir"
+          ${pkgs.weechat}/bin/weechat
+
+          for i in $(find . -name '*.sops'); do
+            ${pkgs.sops}/bin/sops -e "''${i%%.sops}" > "$i"
+            rm -f "''${i%%.sops}"
+          done
+
+          # remove symlinks I added myself
+          rm -rf logs weechat.log urlserver_list.txt
+
+          ${pkgs.rsync}/bin/rsync -auz "$temp_dir/" "$local_dir/"
+
+          cd -
+          rm -rf "$temp_dir"
+        ''
+      );
+
+      # NOTE: Only works well with lingering enabled -- otherwise systemd might kill
+      # the service on logout (aka once there are no more user sessions)
+      # If you need to run this on a server then enable lingering session:
+      #   sudo loginctl enable-linger $USER
+      systemd.user.services."weechat" = {
+        Unit = {
+          Description = "A WeeChat client and relay service using Tmux";
+          After = "network.target";
+          Before = "shutdown.target";
+          Conflicts = "shutdown.target";
+        };
+
+        Service = {
+          Type = "forking";
+          ExecStart = "${pkgs.tmux}/bin/tmux -L weechat new-session -s weechat -d ${pkgs.weechat}/bin/weechat";
+          ExecStartPost = "${pkgs.tmux}/bin/tmux -L weechat set status"; # turn off the status bar
+          ExecStop = "-${pkgs.tmux}/bin/tmux -L weechat kill-server";
+
+          Environment =
+            let
+              secureSocket = config.programs.tmux.enable && config.programs.tmux.secureSocket;
+            in
+            [ "WEECHAT_HOME=${weechat-home}" ]
+            ++ (optionals secureSocket [ "TMUX_TMPDIR=/run/user/2000" ]); # TODO: Do not hardcode my uid
+        };
+
+        Install.WantedBy = [ "default.target" ];
+      };
+    })
+  ]);
+}
