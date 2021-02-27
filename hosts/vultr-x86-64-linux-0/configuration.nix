2b498e812c5eab3e5385cdfe839704721604024a
Wael Nasreddine <wael.nasreddine@gmail.com>
2021-02-27 12:26:31 -0800

    enable wordpress

--- hosts/vultr-x86-64-linux-0/configuration.nix
+++ hosts/vultr-x86-64-linux-0/configuration.nix
@@ -10,5 +10,13 @@ with lib;
   networking.useDHCP = false;
   networking.interfaces.ens3.useDHCP = true;
 
+  soxincfg.services.wordpress.tripintech.enable = true;
+  services.wordpress.tripintech = {
+    virtualHost = {
+      adminAddr = "wael.nasreddine@gmail.com";
+      hostName = "tripin.tech";
+    };
+  };
+
   system.stateVersion = "20.09";
 }
