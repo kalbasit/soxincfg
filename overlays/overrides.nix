c6451f95817e2e2e0be19bbd005ae9650a4197de
Wael Nasreddine <wael.nasreddine@gmail.com>
2021-03-16 21:01:42 -0700

    overlays/overrides: grab jetbrains from master

--- overlays/overrides.nix
+++ overlays/overrides.nix
@@ -1,5 +1,5 @@
 pkgs-master:
 
 final: prev: {
-  inherit (pkgs-master) nixpkgs-fmt starship terraform terraform-providers firefox tailscale;
+  inherit (pkgs-master) nixpkgs-fmt starship terraform terraform-providers firefox tailscale jetbrains;
 }
