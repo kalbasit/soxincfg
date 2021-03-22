bd77dc9925290ce738a57726f6e5d604db620c12
Wael Nasreddine <wael.nasreddine@gmail.com>
2021-03-22 09:00:09 -0700

    profiles/myself: setup VNC to Athena

--- profiles/myself/home-secrets.nix
+++ profiles/myself/home-secrets.nix
@@ -20,6 +20,9 @@ mkMerge [
     sops.secrets._gitconfig_secrets = { inherit owner sopsFile; path = "${yl_home}/.gitconfig.secrets"; };
     sops.secrets._github_token = { inherit owner sopsFile; path = "${yl_home}/.github_token"; };
     sops.secrets._jrnl_config = { inherit owner sopsFile; path = "${yl_home}/.jrnl_config"; };
+    sops.secrets._local_share_remmina_my_network_vnc_athena_athena_admin_nasreddine_com_remmina = { inherit owner sopsFile; path = "${yl_home}/.local/share/remmina/my-network_vnc_athena_athena-admin-nasreddine-com.remmina"; };
+    sops.secrets._local_share_remmina_my_network_vnc_athena_athena_general_nasreddine_com_remmina = { inherit owner sopsFile; path = "${yl_home}/.local/share/remmina/my-network_vnc_athena_athena-general-nasreddine-com.remmina"; };
+    sops.secrets._local_share_remmina_my_network_vnc_poseidon_poseidon_admin_nasreddine_com_remmina = { inherit owner sopsFile; path = "${yl_home}/.local/share/remmina/my-network_vnc_poseidon_poseidon-admin-nasreddine-com.remmina"; };
     sops.secrets._local_share_remmina_my_network_vnc_poseidon_poseidon_general_nasreddine_com_remmina = { inherit owner sopsFile; path = "${yl_home}/.local/share/remmina/my-network_vnc_poseidon_poseidon-general-nasreddine-com.remmina"; };
     sops.secrets._local_share_remmina_my_network_vnc_vanya-macbook_remmina = { inherit owner sopsFile; path = "${yl_home}/.local/share/remmina/my-network_vnc_vanya-macbook.remmina"; };
     sops.secrets._local_share_remmina_my_network_vnc_vanya-macbook_ro_remmina = { inherit owner sopsFile; path = "${yl_home}/.local/share/remmina/my-network_vnc_vanya-macbook-ro.remmina"; };
