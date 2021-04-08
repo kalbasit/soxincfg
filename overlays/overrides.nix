09d47e40514e42c535707cc19862413bf4aaa12f
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2021-04-08 16:46:39 -0700

    overlays/overrides: take a specific idea-ultimate version compatible with the Bazel plugin

--- overlays/overrides.nix
+++ overlays/overrides.nix
@@ -1,5 +1,16 @@
 pkgs-master:
 
 final: prev: {
-  inherit (pkgs-master) nixpkgs-fmt starship terraform terraform-providers firefox tailscale jetbrains;
+  inherit (pkgs-master) nixpkgs-fmt starship terraform terraform-providers firefox tailscale bazel;
+
+  jetbrains = pkgs-master.jetbrains // {
+    idea-ultimate = pkgs-master.jetbrains.idea-ultimate.overrideAttrs(oa: rec {
+      name = "idea-ultimate-${version}";
+      version = "2020.2.4";
+      src = prev.fetchurl {
+        url = "https://download.jetbrains.com/idea/ideaIU-${version}-no-jbr.tar.gz";
+        sha256 = "sha256-/pYbEN7vExfgXEuQy+Sc97h2HzxPlJ3im7VjraJEGRc=";
+      };
+    });
+  };
 }
