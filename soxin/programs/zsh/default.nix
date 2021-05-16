584a8dc21a3db5b893ee6fecc01d0ad4f5ff38cd
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2021-05-15 20:10:53 -0700

    get zsh working

--- soxin/programs/zsh/default.nix
+++ soxin/programs/zsh/default.nix
@@ -205,7 +205,8 @@ in
           '';
 
         plugins = let inherit (pkgs) fetchFromGitHub; in
-          [
+          config.soxin.settings.theme.zsh.plugins
+          ++ [
             {
               name = "enhancd";
               file = "init.sh";
