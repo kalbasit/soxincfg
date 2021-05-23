3a4ac068933abb194a8e1e41deb564722319d1d3
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2021-05-23 15:27:24 -0700

    flake: update all deps
    Greenclip was disabled because it is broken.

--- flake.nix
+++ flake.nix
@@ -2,10 +2,10 @@
   description = "Soxin template flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/4a9b0f0eb450d7bef5db5c05c2086b24aaecf05c";
-    nixpkgs-master.url = "github:NixOS/nixpkgs/4a9b0f0eb450d7bef5db5c05c2086b24aaecf05c";
+    nixpkgs.url = "github:NixOS/nixpkgs/d6f68269def98baf4e85a7732c7d8b537c7a716c";
+    nixpkgs-master.url = "github:NixOS/nixpkgs/d6f68269def98baf4e85a7732c7d8b537c7a716c";
     home-manager = {
-      url = "github:nix-community/home-manager/77188bcd6e2c6c7a99253b36f08ed7b65f2901d2";
+      url = "github:nix-community/home-manager/3d18912f5ae7c98bd5249411d98cdf3b28fe1f09";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     soxin = {
