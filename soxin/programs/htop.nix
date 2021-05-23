9dc7d6427c89ba07fdf2a7ca47e5e4ee365b682e
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2021-05-23 15:42:18 -0700

    programs.htop: fix deprecations

--- soxin/programs/htop.nix
+++ soxin/programs/htop.nix
@@ -20,32 +20,30 @@ in
       programs.htop = {
         enable = true;
 
-        colorScheme = 0;
-        headerMargin = false;
+        settings.color_scheme = 0;
+        settings.header_margin = false;
 
-        highlightBaseName = true;
-        highlightMegabytes = false;
-        highlightThreads = true;
-        showProgramPath = false;
-        showThreadNames = false;
+        settings.highlight_base_name = true;
+        settings.highlight_megabytes = false;
+        settings.highlight_threads = true;
+        settings.show_program_path = false;
+        settings.show_thread_names = false;
 
-        delay = 15;
-        cpuCountFromZero = false;
-        detailedCpuTime = false;
-        hideKernelThreads = false;
-        hideUserlandThreads = false;
-        shadowOtherUsers = false;
-        updateProcessNames = false;
+        settings.delay = 15;
+        settings.cpu_count_from_zero = false;
+        settings.detailed_cpu_time = false;
+        settings.hide_kernel_threads = false;
+        settings.hide_userland_threads = false;
+        settings.shadow_other_users = false;
+        settings.update_process_names = false;
 
-        fields = [ "PID" "USER" "PRIORITY" "NICE" "M_SIZE" "M_RESIDENT" "M_SHARE" "STATE" "PERCENT_CPU" "PERCENT_MEM" "TIME" "COMM" ];
-        meters = {
-          left = [ "LeftCPUs" "Memory" "Swap" "CPU" ];
-          right = [ "RightCPUs" "Tasks" "LoadAverage" "Uptime" ];
-        };
+        settings.fields = [ "PID" "USER" "PRIORITY" "NICE" "M_SIZE" "M_RESIDENT" "M_SHARE" "STATE" "PERCENT_CPU" "PERCENT_MEM" "TIME" "COMM" ];
+        settings.left_meter_modes = [ "LeftCPUs" "Memory" "Swap" "CPU" ];
+        settings.right_meter_modes = [ "RightCPUs" "Tasks" "LoadAverage" "Uptime" ];
 
-        sortDescending = true;
-        sortKey = "PERCENT_CPU";
-        treeView = false;
+        settings.sort_direction = true;
+        settings.sort_key = "PERCENT_CPU";
+        settings.tree_view = false;
       };
     })
   ]);
