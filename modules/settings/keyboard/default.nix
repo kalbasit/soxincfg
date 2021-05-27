5f696e6b49c9ce4bb9f4dfcf2a40ed15bd327724
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2021-05-27 11:25:55 -0700

    move to systemd unit

--- modules/settings/keyboard/default.nix
+++ modules/settings/keyboard/default.nix
@@ -30,6 +30,72 @@ in
 
     (optionalAttrs (mode == "NixOS") (mkIf cfg.zsa.enable  {
       services.udev.packages = singleton pkgs.zsa-auto-us-layout-switcher;
+
+      systemd.services.zsa-auto-us-layout-switcher =
+        let
+          auto-switcher-start = pkgs.writeShellScript "zsa-auto-us-layout-switcher-start" ''
+            set -euo pipefail
+
+            log() {
+              >&2 echo "[zsa-auto-us-layout-switcher] $@"
+            }
+
+            log "Setting the keyboard layout to colemak"
+            setxkbmap -layout us -variant colemak
+
+            log "Setting up the options"
+            setxkbmap -option ctrl:nocaps
+
+            zsa_id=
+            retries=0
+
+            while [[ -z "$zsa_id" ]] && [[ $retries -lt 20 ]]; do
+              log "Getting the ID of the keyboard"
+              zsa_id="$( xinput list | grep 'ZSA Technology Labs Inc ErgoDox EZ Glow' | grep keyboard | grep -v 'Glow Consumer\|Glow System\|Glow Keyboard' | awk -F'=' '{print $2}' | awk '{print $1}' || true )"
+              retries=$(( retries + 1 ))
+              sleep 1
+            done
+
+            if [[ -n "$zsa_id" ]]; then
+              log "Setting up the US layout onto the keyboard $zsa_id"
+              setxkbmap -device "$zsa_id" -layout us
+            else
+              log "No ID were found for the ErgoDox EZ Glow"
+              exit 1
+            fi
+          '';
+
+          auto-switcher-stop = pkgs.writeShellScript "zsa-auto-us-layout-switcher-start" ''
+            set -euo pipefail
+
+            log() {
+              >&2 echo "[zsa-auto-us-layout-switcher] $@"
+            }
+
+            log "Setting the keyboard layout to colemak"
+            setxkbmap -layout us -variant colemak
+
+            log "Setting up the options"
+            setxkbmap -option ctrl:nocaps
+          '';
+        in {
+          after = [ "graphical-session.target" ];
+
+          environment = {
+            Xauthority = "/yl/.Xauthority";
+            DISPLAY = ":0";
+          };
+
+          path = with pkgs; [ gawk xorg.xinput xorg.setxkbmap ];
+
+          serviceConfig = {
+            ExecStart = auto-switcher-start;
+            ExecStop = auto-switcher-stop;
+            RemainAfterExit = true;
+            Type = "oneshot";
+            User = "yl";
+          };
+      };
     }))
 
     (optionalAttrs (mode == "NixOS") {
--- pkgs/zsa-auto-us-layout-switcher/default.nix
+++ pkgs/zsa-auto-us-layout-switcher/default.nix
@@ -3,67 +3,25 @@
 # - https://superuser.com/a/350336/29825
 # - https://github.com/zsa/wally/tree/master/dist/linux64
 
-{ flock, gawk, lib, stdenvNoCC, writeText, writeShellScript, xorg, ... }:
+{ stdenvNoCC, systemd, writeShellScript, writeText, ... }:
 
 let
+  background-systemd-startup = writeShellScript "background-systemd-startup.sh" ''
+    set -euo pipefail
+
+    ${systemd}/bin/systemctl start zsa-auto-us-layout-switcher.service &
+  '';
+
   rule = writeText "60-zsa-auto-us-layout-switcher.rules" ''
     # rule for adding the keyboard
     SUBSYSTEMS=="usb", ACTION=="add", ATTRS{idVendor}=="3297", \
-      ATTRS{idProduct}=="4976", ENV{HOME}="/yl", ENV{DISPLAY}=":0", \
-      ENV{XAUTHORITY}="/yl/.Xauthority", \
-      RUN+="@flock@/bin/flock --nonblock /tmp/add-switch-layout.lock -c @out@/bin/switch-layout"
+      ATTRS{idProduct}=="4976", \
+      RUN{program}="${background-systemd-startup}"
 
     # rule for removing the keyboard
     SUBSYSTEMS=="usb", ACTION=="remove", ATTRS{idVendor}=="3297", \
-      ATTRS{idProduct}=="4976", ENV{HOME}="/yl", ENV{DISPLAY}=":0", \
-      ENV{XAUTHORITY}="/yl/.Xauthority", \
-      RUN+="@flock@/bin/flock --nonblock /tmp/remove-switch-layout.lock -c @out@/bin/switch-layout"
-  '';
-
-  autoswitcher = writeShellScript "zsa-auto-us-layout-switcher" ''
-    set -euo pipefail
-
-    export PATH="${flock}/bin:${gawk}/bin:${xorg.xinput}/bin:${xorg.setxkbmap}/bin:$PATH"
-
-    log() {
-      logger "[zsa-auto-us-layout-switcher] $@"
-    }
-
-    log "Starting as $(whoami) to handle the following action: $ACTION"
-
-    log "Setting the keyboard layout to colemak"
-    setxkbmap -layout us -variant colemak
-
-    log "Setting up the options"
-    setxkbmap -option ctrl:nocaps
-
-    if [[ "$ACTION" == "add" ]]; then
-      # send the loop that configures the keyboard to the background because
-      # the script running blocks xinput from seeing the device and hence it
-      # will never be found.
-      (
-        # allow only one loop to exist
-        flock --nonblock 9 || exit 1
-
-        zsa_id=
-        retries=0
-
-        while [[ -z "$zsa_id" ]] && [[ $retries -lt 20 ]]; do
-          log "Getting the ID of the keyboard"
-          zsa_id="$( xinput list | grep 'ZSA Technology Labs Inc ErgoDox EZ Glow' | grep keyboard | grep -v 'Glow Consumer\|Glow System\|Glow Keyboard' | awk -F'=' '{print $2}' | awk '{print $1}' || true )"
-          retries=$(( retries + 1 ))
-          sleep 1
-        done
-
-        if [[ -n "$zsa_id" ]]; then
-          log "Setting up the US layout onto the keyboard $zsa_id"
-          setxkbmap -device "$zsa_id" -layout us
-        else
-          log "No ID were found for the ErgoDox EZ Glow"
-          exit 1
-        fi
-      ) 9>/tmp/switch-layout-loop.lock &
-    fi
+      ATTRS{idProduct}=="4976", \
+      RUN{program}="${systemd}/bin/systemctl stop zsa-auto-us-layout-switcher.service"
   '';
 in stdenvNoCC.mkDerivation {
   pname = "zsa-auto-us-layout-switcher";
@@ -76,12 +34,8 @@ in stdenvNoCC.mkDerivation {
   unpackPhase = ":";
 
   installPhase = ''
-    mkdir -p $out/bin
-    cp ${autoswitcher} $out/bin/switch-layout
-
     mkdir -p $out/lib/udev/rules.d
     substitute ${rule} $out/lib/udev/rules.d/60-zsa-auto-us-layout.rules \
-      --subst-var-by flock ${flock} \
       --subst-var-by out $out
   '';
 }
