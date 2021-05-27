0f892ea0dad85d3aa3a2de1816a9573b28963345
Wael Nasreddine <wael.nasreddine@gmail.com>
2021-05-27 15:33:35 -0700

    [settings] Set the global keyboard to Colemak; Auto-switch ErgoDox to US (#18)
    All of my workstations are laptops, and they obviously each have their own QWERTY keyboard. I only use Colemak. I recently acquired an ErgoDox which has its own firmware and can define its own layout but it expects the operating system to be configured to use a QWERTY keyboard. So the internal keyboard should be using colemak, and the external keyboard should be using QWERTY; That's a dilemma!
    
    I solved this by writing a udev-triggered script that changes the layout of the external keyboard to QWERTY while keeping the internal keyboard layout set to Colemak.
    
    TODO: Re-implement this using a systemd user unit bound to the device systemd unit. See discussion [here](https://matrix.to/#/!DBFhtjpqmJNENpLDOv:nixos.org/$N3HbP7nmnkpDUprdFrlMO7aMYsHmrDgecTfMlSnigMU?via=nixos.org&via=matrix.org&via=kack.it).

---
+++ modules/settings/keyboard/default.nix
@@ -0,0 +1,106 @@
+{ mode, config, pkgs, lib, ... }:
+
+with lib;
+let
+  cfg = config.soxincfg.settings.keyboard;
+
+  colemakLayout = {
+    x11 = { layout = "us"; variant = "colemak"; };
+    console = { keyMap = "colemak"; };
+  };
+
+  usLayout = {
+    x11 = { layout = "us"; };
+  };
+in
+{
+  options = {
+    soxincfg.settings.keyboard = {
+      enable = mkEnableOption "default settings for keyboard";
+
+      zsa.enable =
+        recursiveUpdate
+        (mkEnableOption "support for keyboards from ZSA like the ErgoDox EZ, Planck EZ and Moonlander Mark I")
+        { default = true; };
+    };
+  };
+
+  config = mkIf cfg.enable (mkMerge [
+    { soxin.settings.keyboard.layouts = singleton colemakLayout; }
+
+    (optionalAttrs (mode == "NixOS") (mkIf cfg.zsa.enable  {
+      environment.systemPackages = with pkgs; [ wally-cli ];
+      hardware.keyboard.zsa.enable = true;
+      services.udev.packages = singleton pkgs.zsa-auto-us-layout-switcher;
+
+      systemd.services.zsa-auto-us-layout-switcher =
+        let
+          auto-switcher-start = pkgs.writeShellScript "zsa-auto-us-layout-switcher-start" ''
+            set -euo pipefail
+
+            log() {
+              >&2 echo "[zsa-auto-us-layout-switcher] $@"
+            }
+
+            log "Setting the keyboard layout to colemak"
+            setxkbmap -layout us -variant colemak
+
+            log "Setting up the options"
+            setxkbmap -option ctrl:nocaps
+
+            zsa_id=
+            retries=0
+
+            while [[ -z "$zsa_id" ]] && [[ $retries -lt 20 ]]; do
+              log "Getting the ID of the keyboard"
+              zsa_id="$( xinput list | grep 'ZSA Technology Labs Inc ErgoDox EZ Glow' | grep keyboard | grep -v 'Glow Consumer\|Glow System\|Glow Keyboard' | awk -F'=' '{print $2}' | awk '{print $1}' || true )"
+              retries=$(( retries + 1 ))
+              sleep 1
+            done
+
+            if [[ -n "$zsa_id" ]]; then
+              log "Setting up the US layout onto the keyboard $zsa_id"
+              setxkbmap -device "$zsa_id" -layout us
+            else
+              log "No ID were found for the ErgoDox EZ Glow"
+              exit 1
+            fi
+          '';
+
+          auto-switcher-stop = pkgs.writeShellScript "zsa-auto-us-layout-switcher-start" ''
+            set -euo pipefail
+
+            log() {
+              >&2 echo "[zsa-auto-us-layout-switcher] $@"
+            }
+
+            log "Setting the keyboard layout to colemak"
+            setxkbmap -layout us -variant colemak
+
+            log "Setting up the options"
+            setxkbmap -option ctrl:nocaps
+          '';
+        in {
+          after = [ "graphical-session.target" ];
+
+          environment = {
+            # TODO: This should be customizable
+            Xauthority = "/yl/.Xauthority";
+            # TODO: This should be customizable
+            DISPLAY = ":0";
+          };
+
+          path = with pkgs; [ gawk xorg.xinput xorg.setxkbmap ];
+
+          serviceConfig = {
+            ExecStart = auto-switcher-start;
+            ExecStop = auto-switcher-stop;
+            RemainAfterExit = true;
+            Type = "oneshot";
+            # TODO: This should be customizable
+            User = "yl";
+          };
+      };
+    }))
+  ]);
+}
--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,3 +1,4 @@
 final: prev: {
   terraform-providers = prev.terraform-providers // { unifi = prev.callPackage ./terraform-provider-unifi.nix { }; };
+  zsa-auto-us-layout-switcher = prev.callPackage ./zsa-auto-us-layout-switcher { };
 }
