e573984b21ed2b618a0377e6d616ec3f79cbbc37
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2021-05-26 22:48:48 -0700

    starting to work on it, not done yet

---
+++ modules/settings/keyboard/default.nix
@@ -0,0 +1,40 @@
+{ mode, config, pkgs, lib, ... }:
+
+with lib;
+let
+  cfg = config.soxincfg.settings.keyboard;
+
+  colemakLayout = {
+    x11 = { layout = "us"; variant = "colemak"; };
+    console = { keyMap = "colemak"; };
+  };
+
+  usLayout = {
+    x11 = { layout = "us"; };
+  };
+in
+{
+  options = {
+    soxincfg.settings.keyboard = {
+      enable = mkEnableOption "default settings for keyboard";
+
+      zsa.enable =
+        recursiveUpdate
+        (mkEnableOption "support for keyboards from ZSA like the ErgoDox EZ, Planck EZ and Moonlander Mark I")
+        { default = true; };
+    };
+  };
+
+  config = mkIf cfg.enable (mkMerge [
+    { soxin.settings.keyboard.layouts = singleton colemakLayout; }
+
+    (mkIf cfg.zsa.enable {
+
+    })
+
+    (optionalAttrs (mode == "NixOS") {
+      hardware.keyboard.zsa.enable = true;
+      environment.systemPackages = with pkgs; [ wally-cli ];
+    })
+  ]);
+}
--- profiles/core/default.nix
+++ profiles/core/default.nix
@@ -17,37 +17,13 @@ let
 in
 {
   config = mkMerge [
-    {
-      soxin.settings = {
-        keyboard = {
-          # ErgoDox has its own way of configuring the layout so long as the
-          # software configures it as US layout. In other words, if I configured the
-          # keyboard to the Colemak layout and done the same to my OS then the
-          # keyboard comes out as an undefined layout (neither Colemak nor US).
-          #
-          # I tried setting up Colemak being the first layout so it remains the
-          # default layout and then add a US layout and for some reason i3 started
-          # looking for Mod4+j to move focus left even though in the configuration
-          # it's clearly configured as Mod4+n
-          #
-          # I tried setting up Colemak being the first layout so it remains the
-          # default layout and then add a US layout and for some reason i3 started
-          # looking for Mod4+j to move focus left even though in the configuration
-          # it's clearly configured as Mod4+n.
-          #
-          # As a workaround, I configured the layouts to be US first then Colemak
-          # but override the defaultLayout to Colemak. With this hack the
-          # encryption password (early console) and the console remains configured
-          # to Colemak but the default layout in i3 is Colemak. I've also configured
-          # Right Ctrl + Right Shift to switch between layouts so I can use both of
-          # my external and internal keyboards.
-          defaultLayout = colemakLayout;
-          layouts = [ usLayout colemakLayout ];
-        };
-        theme = "gruvbox-dark";
-      };
-    }
+    # configure the keyboard
+    { soxincfg.settings.keyboard.enable = true; }
 
+    # configure the theme
+    { soxin.settings.theme = "gruvbox-dark"; }
+
+    # configure NixOS
     (optionalAttrs (mode == "NixOS") {
       nix = {
         package = pkgs.nixFlakes;
