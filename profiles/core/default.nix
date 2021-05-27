231060626a00fabbef5c7c0e0377bfca9ec1db2d
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2021-05-27 10:03:22 -0700

    remove dead code

--- profiles/core/default.nix
+++ profiles/core/default.nix
@@ -5,15 +5,6 @@ let
     url = "https://s3-us-west-1.amazonaws.com/nasreddine-infra/ca.crt";
     sha256 = "17x45njva3a535czgdp5z43gmgwl0lk68p4mgip8jclpiycb6qbl";
   });
-
-  colemakLayout = {
-    x11 = { layout = "us"; variant = "colemak"; };
-    console = { keyMap = "colemak"; };
-  };
-
-  usLayout = {
-    x11 = { layout = "us"; };
-  };
 in
 {
   config = mkMerge [
