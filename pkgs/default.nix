cc294bcd58013cc51e6b649ae6035e576ff214ad
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2021-05-31 14:25:09 -0700

    only include pkgs that fit the system

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,9 +1,15 @@
 channels@{ ... }:
 
 let
-  inherit (channels.nixpkgs) callPackage;
+  inherit (channels.nixpkgs) callPackage lib system;
+  inherit (lib) findSingle filterAttrs platforms;
+
+  pkgs = {
+    terraform-providers-unifi = callPackage ./terraform-provider-unifi.nix { };
+    zsa-auto-us-layout-switcher = callPackage ./zsa-auto-us-layout-switcher { };
+  };
+
+  hasElement = list: elem:
+    (findSingle (x: x == elem) "none" "multiple" list) != "none";
 in
-{
-  terraform-providers-unifi = callPackage ./terraform-provider-unifi.nix { };
-  zsa-auto-us-layout-switcher = callPackage ./zsa-auto-us-layout-switcher { };
-}
+filterAttrs (name: pkg: hasElement (pkg.meta.platforms or platforms.all) system) pkgs
