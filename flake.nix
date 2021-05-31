7f42c6db715f0664b1180402358113f77bca3c8f
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2021-05-31 00:31:17 -0700

    fix the build

--- flake.nix
+++ flake.nix
@@ -76,7 +76,7 @@
 
     in
     soxin.lib.systemFlake {
-      inherit inputs withDeploy withSops nixosModules nixosModule;
+      inherit channels channelsConfig inputs withDeploy withSops nixosModules nixosModule;
 
       # add Soxin's main module to all builders
       extraGlobalModules = [
