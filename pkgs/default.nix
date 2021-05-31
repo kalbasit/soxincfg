b179d0ffc26528b97e4e591920aee28c65901f96
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2021-05-30 23:29:06 -0700

    fix pkgs

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,5 +1,8 @@
-{ callPackage, ... }:
+channels@{ ... }:
 
+let
+  inherit (channels.nixpkgs) callPackage;
+in
 {
   terraform-providers.unifi = callPackage ./terraform-provider-unifi.nix { };
   zsa-auto-us-layout-switcher = callPackage ./zsa-auto-us-layout-switcher { };
