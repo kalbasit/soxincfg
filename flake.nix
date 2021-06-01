79cb7f3d7664ddeddade0c611db310815cfea3a1
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2021-06-01 12:55:38 -0700

    fix the comments that are not relevant anymore

--- flake.nix
+++ flake.nix
@@ -62,12 +62,9 @@
 
       # Default configuration values for `channels.<name>.config = {...}`
       channelsConfig = {
+        # allowBroken = true;
         allowUnfree = true;
-
-        # Set allowBroken and allowUnsupportedSystem so the 'nix flake check' works.
-        # TODO: Why do I have to do this?
-        allowBroken = true;
-        allowUnsupportedSystem = true;
+        # allowUnsupportedSystem = true;
       };
 
       nixosModules = (import ./modules) // {
