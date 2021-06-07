082fba800eb0f91d5e30fd24a574c1c9fef4a6e7
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2021-06-07 17:08:29 +0300

    profiles/work/ulta: add as a new profile

---
+++ profiles/work/ulta/default.nix
@@ -0,0 +1,26 @@
+{ config, home-manager, lib, mode, ... }:
+
+let
+  inherit (lib) mkMerge optionalAttrs;
+in
+{
+  config = mkMerge [
+    {
+      soxin.programs.rbrowser.browsers = {
+        "chromium@ulta" = home-manager.lib.hm.dag.entryBetween [ "brave@personal" ] [ "firefox@ulta" ] { };
+        "firefox@ulta" = home-manager.lib.hm.dag.entryAfter [ "firefox@personal" ] { };
+      };
+    }
+
+    (optionalAttrs (mode == "NixOS") (
+      let
+        yl_home = config.users.users.yl.home;
+        owner = config.users.users.yl.name;
+        sopsFile = ./secrets.sops.yaml;
+      in
+      {
+        sops.secrets._zsh_profiles_ulta_zsh = { inherit owner sopsFile; path = "${yl_home}/.zsh/profiles/ulta.zsh"; };
+      }
+    ))
+  ];
+}
