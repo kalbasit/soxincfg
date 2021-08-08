1381c679d99982bf090ce878e025ca3d794e8ab7
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2021-08-08 07:58:13 -0700

    remove system from builder args

--- hosts/darwins/default.nix
+++ hosts/darwins/default.nix
@@ -4,7 +4,9 @@ let
   # the default channel to follow.
   channelName = "nixpkgs";
   output = "darwinConfigurations";
-  builder = darwin.lib.darwinSystem;
+
+  # Build host with darwinSystem. `removeAttrs` workaround due to https://github.com/LnL7/nix-darwin/issues/319
+  builder = args: darwin.lib.darwinSystem (builtins.removeAttrs args [ "system" ]);
 in
 {
   ###
