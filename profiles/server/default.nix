44881729cc70787acd2dca010913839cfc69ef2c
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2021-08-10 17:52:59 -0700

    unrelated change to pin server profile to indended mode

--- profiles/server/default.nix
+++ profiles/server/default.nix
@@ -1,27 +1,32 @@
-{ config, lib, pkgs, ... }:
+{ config, lib, pkgs, mode, ... }:
 
+with lib;
 with lib;
 
 {
-  # Enable the installation of my neovim
-  # TODO: Need a minimal neovim with just my keybindings
-  # soxincfg.programs.neovim.enable = true;
-  environment.systemPackages = with pkgs; [ neovim ];
+  config = mkMerge [
+    (optionalAttrs (mode == "NixOS") {
+      # Enable the installation of my neovim
+      # TODO: Need a minimal neovim with just my keybindings
+      # soxincfg.programs.neovim.enable = true;
+      environment.systemPackages = with pkgs; [ neovim ];
 
-  # Enable TailScale for zero-config VPN service.
-  services.tailscale.enable = true;
+      # Enable TailScale for zero-config VPN service.
+      services.tailscale.enable = true;
 
-  # Enable SSH server
-  soxin.services.openssh.enable = true;
+      # Enable SSH server
+      soxin.services.openssh.enable = true;
 
-  # Enable eternal-terminal
-  networking.firewall.allowedTCPPorts = singleton config.services.eternal-terminal.port;
-  services.eternal-terminal.enable = true;
+      # Enable eternal-terminal
+      networking.firewall.allowedTCPPorts = singleton config.services.eternal-terminal.port;
+      services.eternal-terminal.enable = true;
 
-  # Setup my keyboard layout
-  soxin.settings.keyboard = {
-    layouts = [
-      { console = { keyMap = "colemak"; }; }
-    ];
-  };
+      # Setup my keyboard layout
+      soxin.settings.keyboard = {
+        layouts = [
+          { console = { keyMap = "colemak"; }; }
+        ];
+      };
+    })
+  ];
 }
