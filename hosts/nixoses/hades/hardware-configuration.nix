c7f96e727cd769f2520f527ab795568977c0da58
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2021-09-13 16:38:02 -0700

    hosts/hades: focusrite driver requires modprobe options

--- hosts/nixoses/hades/hardware-configuration.nix
+++ hosts/nixoses/hades/hardware-configuration.nix
@@ -42,6 +42,7 @@ in
   };
 
   # Fix for Scarlet Focusrite
+  boot.extraModprobeConfig = "options snd_usb_audio vid=0x1235 pid=0x8210 device_setup=1";
   boot.kernelPackages=pkgs.linuxPackages_5_11;
   boot.kernelPatches =
   let
