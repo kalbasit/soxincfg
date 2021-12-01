fa7394220adaa946838cace78cb89cb4da6d2954
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2021-12-01 10:51:19 -0800

    place my private key at the correct location

--- modules/programs/onlykey/default.nix
+++ modules/programs/onlykey/default.nix
@@ -14,8 +14,22 @@ in
       home.packages = with pkgs; [ onlykey onlykey-agent onlykey-cli ];
     })
 
-    (optionalAttrs (mode == "NixOS") {
-      hardware.onlykey.enable = true;
-    })
+    (optionalAttrs (mode == "NixOS") (
+      let
+        yl_home = config.users.users.yl.home;
+        owner = config.users.users.yl.name;
+        sopsFile = ./secrets.sops.yaml;
+        id_ed25519_sk_rk_path = "${yl_home}/.ssh/id_ed25519_sk_rk";
+      in
+      {
+        hardware.onlykey.enable = true;
+        sops.secrets._ssh_id_ed25519_sk_rk = { inherit owner sopsFile; path = id_ed25519_sk_rk_path; };
+
+        soxincfg.programs.ssh = {
+          identitiesOnly = mkDefault true;
+          identityFiles = singleton id_ed25519_sk_rk_path;
+        };
+      }
+    ))
   ]);
 }
