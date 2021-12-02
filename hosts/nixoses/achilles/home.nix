003a802e1700ad35d96ee97bd1a7d6169703cad2
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2021-12-01 17:42:19 -0800

    hosts/nixoses/achilles: Use Termite by default
    WezTerm is not working well on this laptop (it could be a HiDPI issue).
    Moving back to Termite until I can figure this out.

--- hosts/nixoses/achilles/home.nix
+++ hosts/nixoses/achilles/home.nix
@@ -26,4 +26,11 @@ with lib;
     height = 43;
   };
   services.dunst.settings.global.geometry = "600x100-15+58";
+
+  # use Termite on this laptop not sure why WezTerm is not working well (window size is limited).
+  # TODO: fix this
+  xsession.windowManager.i3.config.keybindings = {
+    "Mod4+Return" = mkForce "exec ${getBin pkgs.termite}/bin/termite";
+    "Mod4+Shift+Return" = mkForce "exec ${getBin pkgs.wezterm}/bin/wezterm";
+  };
 }
