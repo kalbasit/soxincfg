ebcf9912df21420bdffce73b99bc1a6466ba3b92
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2021-12-18 10:39:39 -0800

    hardware: Add support for Onlykey to be used for SSH and GnuPG

---
+++ modules/hardware/yubikey/home.nix
@@ -0,0 +1,33 @@
+{ mode, config, pkgs, lib, ... }:
+
+let
+  inherit (lib)
+    mkIf
+    ;
+
+  inherit (pkgs)
+    yubico-piv-tool
+    yubikey-manager
+    yubikey-personalization
+    yubikey-personalization-gui
+    yubioath-desktop
+    ;
+
+  cfg = config.soxincfg.hardware.yubikey;
+in
+{
+  config = mkIf cfg.enable {
+    services.gpg-agent = mkIf cfg.gnupg-support.enable {
+      enable = true;
+
+      enableSshSupport = cfg.gnupg-support.ssh-support.enable;
+      enableExtraSocket = cfg.gnupg-support.extra-socket;
+
+      defaultCacheTtl = cfg.gnupg-support.default-cache-ttl;
+      maxCacheTtl = cfg.gnupg-support.max-cache-ttl;
+
+      defaultCacheTtlSsh = cfg.gnupg-support.ssh-support.default-cache-ttl;
+      maxCacheTtlSsh = cfg.gnupg-support.ssh-support.max-cache-ttl;
+    };
+  };
+}
