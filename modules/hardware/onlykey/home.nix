34408cba0df25d8312386e0b3a19226c081a44e1
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2021-12-30 11:13:34 -0800

    onlykey: place a copy of the public key

--- modules/hardware/onlykey/home.nix
+++ modules/hardware/onlykey/home.nix
@@ -38,6 +38,10 @@ in
         identitiesOnly = mkDefault true;
         identityFiles = singleton "~/.ssh/id_ed25519_sk_rk";
       };
+
+      home.file.".ssh/id_ed25519_sk_rk.pub".text = ''
+        sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAILJKaAMg2OSpXi7+B1oIqzKz1lQiEZPo0Xv6ty35uwzzAAAABHNzaDo= wael.nasreddine+onlykey_0@gmail.com
+      '';
     })
 
     (mkIf cfg.gnupg-support.enable {
