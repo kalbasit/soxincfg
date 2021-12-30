e35f084648cde0ea7a04812f62a93b4835f60c21
Wael Nasreddine <wael.nasreddine@gmail.com>
2021-12-29 17:53:29 -0800

    flake: update flake-utils-plus to v1.3.1 (#24)

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     deploy-rs.url = "github:serokell/deploy-rs";
-    flake-utils-plus.url = "github:gytis-ivaskevicius/flake-utils-plus/v1.1.0";
+    flake-utils-plus.url = "github:gytis-ivaskevicius/flake-utils-plus/v1.3.1";
     home-manager.url = "github:nix-community/home-manager/release-21.11";
     nixos-hardware.url = "github:NixOS/nixos-hardware";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
@@ -65,7 +65,7 @@
       nixosModule = nixosModules.soxincfg;
 
     in
-    soxin.lib.systemFlake {
+    soxin.lib.mkFlake {
       inherit channels channelsConfig inputs withDeploy withSops nixosModules nixosModule;
 
       # add Soxin's main module to all builders
