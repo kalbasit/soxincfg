dc22ae4c15b21453ced208f103e839d94e795ed3
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2022-01-24 19:02:10 -0800

    hosts/achilles: re-enable Nix distributed build and GnuPG

--- hosts/nixoses/achilles/soxincfg.nix
+++ hosts/nixoses/achilles/soxincfg.nix
@@ -14,13 +14,6 @@ in
 
   soxin.hardware.intelBacklight.enable = true;
 
-  # XXX: Temporally disable remote build until SSH is fully configured.
-  soxincfg.settings.nix.distributed-builds.enable = mkForce false;
-
-  # XXX: Temporally disable GnuPG signing until it's fully configured.
-  soxincfg.programs.git.enableGpgSigningKey = mkForce false;
-  soxin.programs.git.gpgSigningKey = mkForce null;
-
   # Setup the name of the wireless interface in Polybar
   soxin.services.xserver.windowManager.bar.modules.network.wlan = singleton "wlp82s0";
 }
