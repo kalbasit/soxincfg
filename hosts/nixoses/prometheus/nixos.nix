64a9f9b6341dd99f7fac401dfa2e4f821acb23a1
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2022-03-16 18:18:50 -0700

    hosts/prometheus: Configure my profile on Prometheus

--- hosts/nixoses/prometheus/nixos.nix
+++ hosts/nixoses/prometheus/nixos.nix
@@ -4,10 +4,16 @@ let
 in
 {
   imports = [
+    soxincfg.nixosModules.profiles.myself
     soxincfg.nixosModules.profiles.server
 
     ./hardware-configuration.nix
   ];
+
+  # load YL's home-manager configuration
+  home-manager.users.yl = import ./home.nix { inherit soxincfg; };
+
+  soxin.hardware.lowbatt.enable = true;
 
   sops.secrets = {
     "networking.wireless.environmentFile" = { inherit sopsFile; };
