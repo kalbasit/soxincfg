030f9392815155e95d464d0ac9d9265cde719ca3
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2022-03-19 11:51:21 -0700

    terraform: define apollo.nasreddine.com

--- terraform/aws/account_1/route53.nasreddine.com.tf
+++ terraform/aws/account_1/route53.nasreddine.com.tf
@@ -192,3 +192,11 @@ resource "aws_route53_record" "unifi-nasreddine-com-cname" {
   ttl     = "60"
   records = ["prometheus.tailscale.nasreddine.com"]
 }
+
+resource "aws_route53_record" "apollo-nasreddine-com-cname" {
+  zone_id = aws_route53_zone.nasreddine-com.zone_id
+  name    = "apollo.nasreddine.com"
+  type    = "CNAME"
+  ttl     = "60"
+  records = ["prometheus.tailscale.nasreddine.com"]
+}
