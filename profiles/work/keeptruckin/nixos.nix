03815e0c372c84be9076c22740e38b274cf22832
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2022-04-15 12:07:27 -0700

    profiles/work/keeptruckin: revisit machines

--- profiles/work/keeptruckin/nixos.nix
+++ profiles/work/keeptruckin/nixos.nix
@@ -26,12 +26,14 @@ in
     _etc_NetworkManager_system-connections_KeepTruckin-Lahore-PK-Office-VPN_nmconnection = { inherit sopsFile; path = "/etc/NetworkManager/system-connections/KeepTruckin-Lahore-PK-Office-VPN.nmconnection"; };
     _etc_NetworkManager_system-connections_KeepTruckin-VPN_nmconnection = { inherit sopsFile; path = "/etc/NetworkManager/system-connections/KeepTruckin-VPN.nmconnection"; };
     _etc_NetworkManager_system-connections_KeepTruckin_nmconnection = { inherit sopsFile; path = "/etc/NetworkManager/system-connections/KeepTruckin.nmconnection"; };
+    _local_share_remmina_keeptruckin_vnc_aarch64-darwin-0_remmina = { inherit owner sopsFile; path = "${yl_home}/.local/share/remmina/keeptruckin_vnc_aarch64-darwin-0.remmina"; };
+    _local_share_remmina_keeptruckin_vnc_aarch64-darwin-1_remmina = { inherit owner sopsFile; path = "${yl_home}/.local/share/remmina/keeptruckin_vnc_aarch64-darwin-1.remmina"; };
     _local_share_remmina_keeptruckin_vnc_mac-devprod_remmina = { inherit owner sopsFile; path = "${yl_home}/.local/share/remmina/keeptruckin_vnc_mac-devprod.remmina"; };
-    _local_share_remmina_keeptruckin_vnc_poseidon_tailscale_nasreddine_com_remmina = { inherit owner sopsFile; path = "${yl_home}/.local/share/remmina/keeptruckin_vnc_poseidon_tailscale_nasreddine_com.remmina"; };
-    _local_share_remmina_keeptruckin_vnc_semele_tailscale_nasreddine_com_remmina = { inherit owner sopsFile; path = "${yl_home}/.local/share/remmina/keeptruckin_vnc_semele_tailscale_nasreddine_com.remmina"; };
-    _local_share_remmina_keeptruckin_vnc_saturn_tailscale_nasreddine_com_remmina = { inherit owner sopsFile; path = "${yl_home}/.local/share/remmina/keeptruckin_vnc_saturn_tailscale_nasreddine_com.remmina"; };
-    _ssh_per-host_ktdev_io_rsa = { inherit owner sopsFile; path = "${yl_home}/.ssh/per-host/ktdev.io_rsa"; };
+    _local_share_remmina_keeptruckin_vnc_x86-64-darwin-0_remmina = { inherit owner sopsFile; path = "${yl_home}/.local/share/remmina/keeptruckin_vnc_x86-64-darwin-0.remmina"; };
+    _local_share_remmina_keeptruckin_vnc_x86-64-darwin-1_remmina = { inherit owner sopsFile; path = "${yl_home}/.local/share/remmina/keeptruckin_vnc_x86-64-darwin-1.remmina"; };
+    _local_share_remmina_keeptruckin_vnc_x86-64-darwin-2_remmina = { inherit owner sopsFile; path = "${yl_home}/.local/share/remmina/keeptruckin_vnc_x86-64-darwin-2.remmina"; };
     _ssh_config_include_work_keeptruckin = { inherit owner sopsFile; path = "${yl_home}/.ssh/config_include_work_keeptruckin"; };
+    _ssh_per-host_ktdev_io_rsa = { inherit owner sopsFile; path = "${yl_home}/.ssh/per-host/ktdev.io_rsa"; };
     _zsh_profiles_keeptruckin_admin_zsh = { inherit owner sopsFile; path = "${yl_home}/.zsh/profiles/keeptruckin.admin.zsh"; };
     _zsh_profiles_keeptruckin_playground_zsh = { inherit owner sopsFile; path = "${yl_home}/.zsh/profiles/keeptruckin.playground.zsh"; };
     _zsh_profiles_keeptruckin_zsh = { inherit owner sopsFile; path = "${yl_home}/.zsh/profiles/keeptruckin.zsh"; };
