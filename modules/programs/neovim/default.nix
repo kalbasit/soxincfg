62cd8cafb07ab88b17971f6c5aef084d71493f04
Wael Nasreddine <wael.nasreddine@gmail.com>
2022-04-25 21:24:33 -0700

    EMPTY VIM
    (cherry picked from commit 52867c581f0c15d1711de34a330a9a6ce5357873)

--- modules/programs/neovim/default.nix
+++ modules/programs/neovim/default.nix
@@ -63,56 +63,38 @@ in
       ];
 
       plugins = with pkgs.vimPlugins; [
+        # repeat # is this still needed?
         LanguageClient-neovim
         PreserveNoEOL
         ack-vim
-        # ale
         auto-pairs
         caw
         csv-vim
-        # deoplete-go
-        # deoplete-nvim
         direnv-vim
         easy-align
         easymotion
-        editorconfig-vim
         emmet-vim
         fzf-vim
         fzfWrapper
-        goyo
+        goyo # :Goyo for distraction-free writin
         gundo-vim
         multiple-cursors
-        ncm2
-        pig-vim
         registers-nvim
-        repeat
-        rhubarb
-        sleuth
+        rhubarb # GitHub support for Fugitive
+        sleuth # shiftwidth/expandtab settings. Replaces EditorConfig.
         surround
         traces-vim
         vim-airline
         vim-airline-themes
         vim-better-whitespace
-        vim-clang-format
         vim-eunuch
         vim-fugitive
         vim-gist
-        vim-go
-        vim-markdown
-        vim-scala
         vim-signify
         vim-speeddating
-        vim-terraform
-        vimtex
         vissort-vim
-        webapi-vim # required for Gist
-        yats-vim
+        webapi-vim # required for vim-gist
         zoomwintab-vim
-
-        # NOTE: polyglot must be loaded manually because if it gets loaded
-        # before vim-go and conflicts with it causing problems starting nvim.
-        # See https://github.com/sheerun/vim-polyglot/issues/309
-        { plugin = polyglot; optional = true; }
       ]
       ++ (optionals (keyboardLayout == "colemak") [ vim-colemak ]);
     };
