fb7977a7dad34cd60cc176107117e4222dea8c6c
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2022-10-18 15:50:12 -0700

    home: disable xautolock and use xset instead
    My lock-screen keeps trying to lock whilst it's already locked, for some
    reason, and that keeps all of my monitors on all the time.

--- profiles/work/ulta/nixos.nix
+++ profiles/work/ulta/nixos.nix
@@ -6,10 +6,10 @@ let
   sopsFile = ./secrets.sops.yaml;
 in
 {
-  sops.secrets={
-  _aws_configure_profile_ulta_sh = { inherit owner sopsFile; mode = "0500"; };
-  _ssh_config_include_work_ulta = { inherit owner sopsFile; path = "${yl_home}/.ssh/config_include_work_ulta"; };
-  _ssh_per-host_linnaeus_io_rsa = { inherit owner sopsFile; path = "${yl_home}/.ssh/per-host/linnaeus.io_rsa"; };
-  _zsh_profiles_ulta_zsh = { inherit owner sopsFile; path = "${yl_home}/.zsh/profiles/ulta.zsh"; };
+  sops.secrets = {
+    _aws_configure_profile_ulta_sh = { inherit owner sopsFile; mode = "0500"; };
+    _ssh_config_include_work_ulta = { inherit owner sopsFile; path = "${yl_home}/.ssh/config_include_work_ulta"; };
+    _ssh_per-host_linnaeus_io_rsa = { inherit owner sopsFile; path = "${yl_home}/.ssh/per-host/linnaeus.io_rsa"; };
+    _zsh_profiles_ulta_zsh = { inherit owner sopsFile; path = "${yl_home}/.zsh/profiles/ulta.zsh"; };
   };
 }
