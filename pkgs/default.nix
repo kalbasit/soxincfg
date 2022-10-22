ff85bc2eb5960c7988a56d89350092f30eaef38c
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2022-10-22 10:46:21 -0700

    keyboard: remove zsa auto-switcher as it does not work well anyways
    I believe that it's causing more harm than it is doing good. I fear that
    my issues with the lock screen are coming from this functionality.

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -6,7 +6,6 @@ let
 
   pkgs = {
     terraform-providers-unifi = callPackage ./terraform-provider-unifi.nix { };
-    zsa-auto-us-layout-switcher = callPackage ./zsa-auto-us-layout-switcher { };
   };
 
   hasElement = list: elem:
