beb41f82527c1381b8c0607c94608571f4c0cefb
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2022-10-22 10:42:31 -0700

    Revert "home: disable xautolock and use xset instead"
    This reverts commit fb7977a7dad34cd60cc176107117e4222dea8c6c.

--- profiles/workstation/nixos/local/home.nix
+++ profiles/workstation/nixos/local/home.nix
@@ -5,17 +5,8 @@
   services.flameshot.enable = true;
   services.betterlockscreen = {
     enable = true;
-    inactiveInterval = 5;
     arguments = [
       "--show-layout"
     ];
-  };
-
-  services.screen-locker = {
-    enable = true;
-    xss-lock = {
-      screensaverCycle = 300;
-    };
-    xautolock = { enable = false; };
   };
 }
