c19baa756850df917fed6cf7840f2375d0f8ed26
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2022-10-25 11:06:43 -0700

    hosts/kore: remove the extra boot configuration

--- hosts/nixoses/kore/hardware-configuration.nix
+++ hosts/nixoses/kore/hardware-configuration.nix
@@ -6,8 +6,6 @@
 
   # Use the extlinux boot loader. (NixOS wants to enable GRUB by default)
   boot.loader.grub.enable = false;
-  # Enables the generation of /boot/extlinux/extlinux.conf
-  boot.loader.generic-extlinux-compatible.enable = true;
 
   boot.loader.raspberryPi.enable = true;
   boot.loader.raspberryPi.version = 3;
