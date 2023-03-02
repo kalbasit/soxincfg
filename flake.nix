0799045fe3f4728dad24e24fa543cd42f85b86a6
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2023-03-02 12:55:58 -0800

    flake: patch nixpkgs with NixOS/nixpkgs#219211

--- flake.nix
+++ flake.nix
@@ -57,7 +57,9 @@
           config = { };
 
           # Yep, you see it first folks - you can patch nixpkgs!
-          patches = [ ];
+          patches = [
+            ./patches/219211-patch-execs-in-libagent.patch
+          ];
         };
       };
 
