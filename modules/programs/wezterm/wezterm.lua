3b7da13ba57c0649ad74cfe1d866755178fd0e75
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2023-03-02 22:29:49 -0800

    programs.wezterm: add a gradient background

--- modules/programs/wezterm/wezterm.lua
+++ modules/programs/wezterm/wezterm.lua
@@ -23,4 +23,55 @@ return {
   check_for_updates = false, -- since it's installed by Nix, let Nix manage its updates.
   color_scheme = "Gruvbox Dark",
   font_size = 10,
+
+  window_background_gradient = {
+    -- Can be "Vertical" or "Horizontal".  Specifies the direction
+    -- in which the color gradient varies.  The default is "Horizontal",
+    -- with the gradient going from left-to-right.
+    -- Linear and Radial gradients are also supported; see the other
+    -- examples below
+    orientation = 'Vertical',
+
+    -- Specifies the set of colors that are interpolated in the gradient.
+    -- Accepts CSS style color specs, from named colors, through rgb
+    -- strings and more
+    colors = {
+      '#0f0c29',
+      '#302b63',
+      '#24243e',
+    },
+
+    -- Instead of specifying `colors`, you can use one of a number of
+    -- predefined, preset gradients.
+    -- A list of presets is shown in a section below.
+    -- preset = "Warm",
+
+    -- Specifies the interpolation style to be used.
+    -- "Linear", "Basis" and "CatmullRom" as supported.
+    -- The default is "Linear".
+    interpolation = 'Linear',
+
+    -- How the colors are blended in the gradient.
+    -- "Rgb", "LinearRgb", "Hsv" and "Oklab" are supported.
+    -- The default is "Rgb".
+    blend = 'Rgb',
+
+    -- To avoid vertical color banding for horizontal gradients, the
+    -- gradient position is randomly shifted by up to the `noise` value
+    -- for each pixel.
+    -- Smaller values, or 0, will make bands more prominent.
+    -- The default value is 64 which gives decent looking results
+    -- on a retina macbook pro display.
+    -- noise = 64,
+
+    -- By default, the gradient smoothly transitions between the colors.
+    -- You can adjust the sharpness by specifying the segment_size and
+    -- segment_smoothness parameters.
+    -- segment_size configures how many segments are present.
+    -- segment_smoothness is how hard the edge is; 0.0 is a hard edge,
+    -- 1.0 is a soft edge.
+
+    -- segment_size = 11,
+    -- segment_smoothness = 0.0,
+  },
 }
