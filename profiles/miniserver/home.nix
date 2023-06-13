9fb8d153a507b5b05c7d91be2c6d44e87089a18d
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2023-06-13 15:40:00 -0700

    hosts/kore: use a smaller profile for this server - miniserver

---
+++ profiles/miniserver/home.nix
@@ -0,0 +1,22 @@
+{ config, lib, pkgs, ... }:
+
+let
+  inherit (lib)
+    optionals
+    ;
+in
+{
+  home.packages = with pkgs; [
+    binutils # for strings
+    dnsutils # for dig
+    screen
+    duf # du replacement on steroids
+    ncdu
+    file
+    gnupg
+    jq
+    killall
+    nix-zsh-completions
+    unzip
+  ];
+}
---
+++ profiles/miniserver/nixos.nix
@@ -0,0 +1,33 @@
+{ config, lib, pkgs, ... }:
+
+let
+  inherit (lib)
+    singleton
+    ;
+in
+{
+  environment.homeBinInPath = true;
+
+  # Feed the kernel some entropy
+  services.haveged.enable = true;
+
+  # Enable the installation of my neovim
+  # TODO: Need a minimal neovim with just my keybindings
+  # soxincfg.programs.neovim.enable = true;
+  environment.systemPackages = with pkgs; [ neovim ];
+
+  # Enable TailScale for zero-config VPN service.
+  services.tailscale.enable = true;
+
+  # Enable eternal-terminal
+  # TODO: I have not used this. Do I need it?
+  # networking.firewall.allowedTCPPorts = singleton config.services.eternal-terminal.port;
+  # services.eternal-terminal.enable = true;
+
+  # Setup my keyboard layout
+  soxin.settings.keyboard = {
+    layouts = [
+      { console = { keyMap = "colemak"; }; }
+    ];
+  };
+}
