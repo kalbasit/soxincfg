c13394f7908f8f7a62e21c5261d8798b684074d6
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2023-07-05 09:51:44 -0700

    hosts/prometheus: integrate rtl_433 over mosquito with auto discovery

--- hosts/nixoses/prometheus/containers.nix
+++ hosts/nixoses/prometheus/containers.nix
@@ -56,8 +56,15 @@ in
       volumes = [ "/persistence/octoprint:/octoprint" ];
     };
 
+    containers.mosquitto = {
+      image = "eclipse-mosquitto:2.0.15-openssl@sha256:9f39fb6724428c717e598522417010d5799409a761144175ed8f107e8827d381";
+      extraOptions = [ "--network=${network_name}" ];
+      volumes = [ "/persistence/mosquitto:/mosquitto" ];
+      ports = [ "1883:1883" ];
+    };
+
     containers.homeassistant = {
-      dependsOn = [ "zwave-js" ];
+      dependsOn = [ "mosquitto" "zwave-js" ];
       environment.TZ = config.time.timeZone;
       extraOptions = [ "--network=${network_name}" ];
       image = "ghcr.io/home-assistant/home-assistant:2023.6.1@sha256:a29527b65f76ca36451aed4a4ae8859821c54e4c6d6ec70a7a6d547adb6fd2b9";
@@ -66,7 +73,10 @@ in
 
     containers.zwave-js = {
       environment.TZ = config.time.timeZone;
-      extraOptions = [ "--network=${network_name}" "--device=/dev/serial/by-id/usb-0658_0200-if00:/dev/zwave" ];
+      extraOptions = [
+        "--network=${network_name}"
+        "--device=/dev/serial/by-id/usb-0658_0200-if00:/dev/zwave"
+      ];
       image = "zwavejs/zwave-js-ui:8.19.0@sha256:94d46355d3f930579d028114117b8339d8e88181c8ee1d6ed9b9bad74fb74708";
       volumes = [ "/persistence/zwave-js:/usr/src/app/store" ];
     };
@@ -77,6 +87,32 @@ in
       environment.MODE = "native";
       environment.AUTO_RECEIVE_SCHEDULE = "0 22 * * *";
       volumes = [ "/persistence/signal-cli-rest-api:/home/.local/share/signal-cli" ];
+    };
+
+    containers.rtl-433 = {
+      cmd = [
+        "-Mtime:unix:usec:utc"
+        "-Mbits"
+        "-Mlevel"
+        "-Mprotocol"
+        "-Mstats:2:300"
+        "-Fmqtt://mosquitto:1883,retain=1"
+      ];
+      dependsOn = [ "mosquitto" ];
+      image = "hertzg/rtl_433:21.12@sha256:9751f2c1561af0069cbfb67b675d9000b1fb9a5fff8d24026c2b59f5959fda55";
+      extraOptions = [
+        "--network=${network_name}"
+        "--device=/dev/bus/usb/001/007"
+      ];
+    };
+
+    containers.rtl-433-mqtt-autodiscovery = {
+      dependsOn = [ "mosquitto" ];
+      image = "ghcr.io/pbkhrv/rtl_433-hass-addons-rtl_433_mqtt_autodiscovery-amd64:0.6.0@sha256:172e9636ed002bc5f8b6527ee4c488172ad14737a09e8b50f6f768e15f460774";
+      extraOptions = [ "--network=${network_name}" ];
+      environment.MQTT_HOST = "mosquitto";
+      environment.MQTT_PORT = "1883";
+      environment.DISCOVERY_INTERVAL = "60";
     };
   };
 }
