daf4cc0304244cbf73e94b93b3cc4fde238b208c
Wael Nasreddine <wael.nasreddine@gmail.com>
2023-08-31 13:29:47 -0700

    treewide: configure sops with granual encryption

--- scripts/sops-updatekeys.sh
+++ scripts/sops-updatekeys.sh
@@ -3,25 +3,12 @@ set -euo pipefail
 
 readonly root_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd )"
 
-if [[ ! -v IN_NIX_SHELL  ]] || [[ ! -v SOPS_PGP_FP ]]; then
+if [[ ! -v IN_NIX_SHELL ]]; then
     >&2 echo "FATAL: You must run this from within soxincfg devShell. TIP: Use Direnv to automatically load it for you."
     exit 1
 fi
 
 cd "$root_dir"
 
-rm -f ".sops.yaml"
-
-readonly sops_files=($(find . -name '*.sops' -print) $(find . -name '*.sops.*' -print))
-
-cat <<EOF > .sops.yaml
-creation_rules:
-- pgp: $SOPS_PGP_FP
-EOF
-
-for fn in "${sops_files[@]}"; do
-    >&2 echo ">>> Updating $fn"
-    sops updatekeys -y "$fn"
-done
-
-rm -f ".sops.yaml"
+find . \( \( -name '*.sops.yaml' -or -name '*.sops.yml' -or -name '*.sops.json' -or -name '*.sops' \) -and -not \( -name '.sops.yaml' \) \) -print0 |
+    xargs -0L1 sops updatekeys --yes
