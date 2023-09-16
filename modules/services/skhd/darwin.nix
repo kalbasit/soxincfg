2ed5f5fe4e0a2da8331801b13be9b4ccd300812b
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2023-09-15 18:19:05 -0700

    services/skhd: add more keybindings based on Felix Katz configs
    https://github.com/FelixKratz/dotfiles/blob/f3e9c4e604a1132c68787f130843d0aa750ee54e/.config/skhd/skhdrc

--- modules/services/skhd/darwin.nix
+++ modules/services/skhd/darwin.nix
@@ -18,19 +18,33 @@ in
       enable = true;
       skhdConfig = ''
         # focus window
-        alt - n : yabai -m window --focus west
-        alt - e : yabai -m window --focus south
-        alt - i : yabai -m window --focus north
-        alt - o : yabai -m window --focus east
+        alt - n : yabai -m window --focus west || yabai -m display --focus west
+        alt - e : yabai -m window --focus south || yabai -m display --focus south
+        alt - i : yabai -m window --focus north || yabai -m display --focus north
+        alt - o : yabai -m window --focus east || yabai -m display --focus east
+
+        # Float / Unfloat window: lalt - space
+        alt - space : yabai -m window --toggle float; sketchybar --trigger window_focus
 
         # swap window
-        shift + alt - n : yabai -m window --swap west
-        shift + alt - e : yabai -m window --swap south
-        shift + alt - i : yabai -m window --swap north
-        shift + alt - o : yabai -m window --swap east
+        shift + alt - n : yabai -m window --warp west || $(yabai -m window --display west && sketchybar --trigger windows_on_spaces && yabai -m display --focus west && yabai -m window --warp last) || yabai -m window --move rel:-10:0
+        shift + alt - e : yabai -m window --warp south || $(yabai -m window --display south && sketchybar --trigger windows_on_spaces && yabai -m display --focus south) || yabai -m window --move rel:0:10
+        shift + alt - i : yabai -m window --warp north || $(yabai -m window --display north && sketchybar --trigger windows_on_spaces && yabai -m display --focus north) || yabai -m window --move rel:0:-10
+        shift + alt - o : yabai -m window --warp east || $(yabai -m window --display east && sketchybar --trigger windows_on_spaces && yabai -m display --focus east && yabai -m window --warp first) || yabai -m window --move rel:10:0
 
         # toggle window zoom
-        alt - f : yabai -m window --toggle zoom-fullscreen
+        alt - f : yabai -m window --toggle zoom-parent; sketchybar --trigger window_focus
+        shift + alt - f : yabai -m window --toggle zoom-fullscreen; sketchybar --trigger window_focus
+
+        # Equalize size of windows: ctrl + alt - e
+        ctrl + alt - e : yabai -m space --balance
+
+        # Insertion (shift + ctrl + alt - ...)
+        # Set insertion point for focused container: shift + ctrl + alt - {n, e, i, o}
+        shift + ctrl + lalt - n : yabai -m window --insert west
+        shift + ctrl + lalt - e : yabai -m window --insert south
+        shift + ctrl + lalt - i : yabai -m window --insert north
+        shift + ctrl + lalt - o : yabai -m window --insert east
       '';
     };
   };
