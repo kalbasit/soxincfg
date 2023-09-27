b6acdca4626298760eba695b6c74524799db614c
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2023-09-26 20:23:27 -0700

    services/skhd: Restart service upon config changes

--- modules/services/skhd/home-manager.nix
+++ modules/services/skhd/home-manager.nix
@@ -5,50 +5,51 @@ let
     mkIf
     ;
 
-  cfg = config.soxincfg.services.yabai;
+  cfg = config.soxincfg.services.skhd;
 in
 {
   config = mkIf cfg.enable {
+    xdg.configFile."skhd/skhdrc" = {
+      onChange = ''
+        if ! test -f ${config.home.homeDirectory}/Library/LaunchAgents/com.koekeishiya.skhd.plist; then
+          skhd --install-service
+          skhd --start-service
+        else
+          skhd --stop-service || true
+          skhd --start-service
+        fi
+      '';
 
-    # Must run these commands after installing for the first time
-    #
-    #   skhd --install-service
-    #   skhd --start-service
-    #
-    # And this commands to update
-    #
-    #   skhd --restart-service
-    #
-    # TODO: Automate this!
-    xdg.configFile."skhd/skhdrc".text = ''
-      # focus window
-      alt - n : yabai -m window --focus west || yabai -m display --focus west
-      alt - e : yabai -m window --focus south || yabai -m display --focus south
-      alt - i : yabai -m window --focus north || yabai -m display --focus north
-      alt - o : yabai -m window --focus east || yabai -m display --focus east
+      text = ''
+        # focus window
+        alt - n : yabai -m window --focus west || yabai -m display --focus west
+        alt - e : yabai -m window --focus south || yabai -m display --focus south
+        alt - i : yabai -m window --focus north || yabai -m display --focus north
+        alt - o : yabai -m window --focus east || yabai -m display --focus east
 
-      # Float / Unfloat window: lalt - space
-      alt - space : yabai -m window --toggle float; sketchybar --trigger window_focus
+        # Float / Unfloat window: lalt - space
+        alt - space : yabai -m window --toggle float; sketchybar --trigger window_focus
 
-      # swap window
-      shift + alt - n : yabai -m window --warp west || $(yabai -m window --display west && sketchybar --trigger windows_on_spaces && yabai -m display --focus west && yabai -m window --warp last) || yabai -m window --move rel:-10:0
-      shift + alt - e : yabai -m window --warp south || $(yabai -m window --display south && sketchybar --trigger windows_on_spaces && yabai -m display --focus south) || yabai -m window --move rel:0:10
-      shift + alt - i : yabai -m window --warp north || $(yabai -m window --display north && sketchybar --trigger windows_on_spaces && yabai -m display --focus north) || yabai -m window --move rel:0:-10
-      shift + alt - o : yabai -m window --warp east || $(yabai -m window --display east && sketchybar --trigger windows_on_spaces && yabai -m display --focus east && yabai -m window --warp first) || yabai -m window --move rel:10:0
+        # swap window
+        shift + alt - n : yabai -m window --warp west || $(yabai -m window --display west && sketchybar --trigger windows_on_spaces && yabai -m display --focus west && yabai -m window --warp last) || yabai -m window --move rel:-10:0
+        shift + alt - e : yabai -m window --warp south || $(yabai -m window --display south && sketchybar --trigger windows_on_spaces && yabai -m display --focus south) || yabai -m window --move rel:0:10
+        shift + alt - i : yabai -m window --warp north || $(yabai -m window --display north && sketchybar --trigger windows_on_spaces && yabai -m display --focus north) || yabai -m window --move rel:0:-10
+        shift + alt - o : yabai -m window --warp east || $(yabai -m window --display east && sketchybar --trigger windows_on_spaces && yabai -m display --focus east && yabai -m window --warp first) || yabai -m window --move rel:10:0
 
-      # toggle window zoom
-      alt - f : yabai -m window --toggle zoom-parent; sketchybar --trigger window_focus
-      shift + alt - f : yabai -m window --toggle zoom-fullscreen; sketchybar --trigger window_focus
+        # toggle window zoom
+        alt - f : yabai -m window --toggle zoom-parent; sketchybar --trigger window_focus
+        shift + alt - f : yabai -m window --toggle zoom-fullscreen; sketchybar --trigger window_focus
 
-      # Equalize size of windows: ctrl + alt - e
-      ctrl + alt - e : yabai -m space --balance
+        # Equalize size of windows: ctrl + alt - e
+        ctrl + alt - e : yabai -m space --balance
 
-      # Insertion (shift + ctrl + alt - ...)
-      # Set insertion point for focused container: shift + ctrl + alt - {n, e, i, o}
-      shift + ctrl + lalt - n : yabai -m window --insert west
-      shift + ctrl + lalt - e : yabai -m window --insert south
-      shift + ctrl + lalt - i : yabai -m window --insert north
-      shift + ctrl + lalt - o : yabai -m window --insert east
-    '';
+        # Insertion (shift + ctrl + alt - ...)
+        # Set insertion point for focused container: shift + ctrl + alt - {n, e, i, o}
+        shift + ctrl + lalt - n : yabai -m window --insert west
+        shift + ctrl + lalt - e : yabai -m window --insert south
+        shift + ctrl + lalt - i : yabai -m window --insert north
+        shift + ctrl + lalt - o : yabai -m window --insert east
+      '';
+    };
   };
 }
