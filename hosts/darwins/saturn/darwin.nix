7322f4a63c9c33130124e1076ac88c25c666b1f1
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2023-10-19 17:02:15 -0700

    hosts/darwin/saturn: install LaunchAgent to auto-backup Obsidian vaults

--- hosts/darwins/saturn/darwin.nix
+++ hosts/darwins/saturn/darwin.nix
@@ -15,6 +15,33 @@ in
   # load YL's home-manager configuration
   home-manager.users.yl = import ./home.nix { inherit soxincfg; };
 
+  # Auto-Backup Obsidian Vaults and send them to GitHub
+  launchd.user.agents.auto-backup-obsidian-vaults = {
+    path = [ config.environment.systemPath ];
+
+    serviceConfig.ProgramArguments =
+      let
+        script = pkgs.writeShellScript "auto-backup-obsidian-vaults" ''
+          set -euo pipefail
+
+          cd ~/SynologyDrive/Obsidian
+
+          while true
+          do
+            git add -A .
+            git commit --no-gpg-sign -m 'Auto-Backup from user agent auto-backup-obsidian-vaults'
+            git push origin main
+
+            sleep 5m
+          done
+        '';
+      in
+      singleton (builtins.toString script);
+
+    serviceConfig.KeepAlive = true;
+    serviceConfig.ProcessType = "Interactive";
+  };
+
   # TODO: Make gpg work, and re-enable this.
   soxincfg.programs.git.enableGpgSigningKey = false;
 
