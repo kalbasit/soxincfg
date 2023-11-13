d5fa606cc764058ad8d89a8aec88570e9281f0c7
Felix Kratz <felix.kratz@tu-dortmund.de>
2023-11-13 14:47:08 +0100

    update sketchybar config for v2.19.3 (native window events)
    (cherry picked from commit 4e36f1619387b10aaff3133f228f63016f260002)

--- modules/services/yabai/home-manager.nix
+++ modules/services/yabai/home-manager.nix
@@ -44,12 +44,12 @@ in
         yabai -m config active_window_border_color 0xffe1e3e4
         yabai -m config active_window_opacity 1.0
         yabai -m config auto_balance off
-        yabai -m config bottom_padding 4
+        yabai -m config bottom_padding 8
         yabai -m config external_bar all:35:0
         yabai -m config focus_follows_mouse off
         yabai -m config insert_feedback_color 0xff9dd274
         yabai -m config layout bsp
-        yabai -m config left_padding 4
+        yabai -m config left_padding 8
         yabai -m config mouse_action1 move
         yabai -m config mouse_action2 resize
         yabai -m config mouse_drop_action swap
@@ -57,16 +57,16 @@ in
         yabai -m config mouse_modifier fn
         yabai -m config normal_window_border_color 0xff494d64
         yabai -m config normal_window_opacity 0.0
-        yabai -m config right_padding 4
+        yabai -m config right_padding 8
         yabai -m config split_ratio 0.50
-        yabai -m config top_padding 10
+        yabai -m config top_padding 14
         yabai -m config window_animation_duration 0.1
         yabai -m config window_border on
         yabai -m config window_border_blur off
         yabai -m config window_border_hidpi off
         yabai -m config window_border_radius 11
         yabai -m config window_border_width 2
-        yabai -m config window_gap 6
+        yabai -m config window_gap 10
         yabai -m config window_opacity off
         yabai -m config window_opacity_duration 0.0
         yabai -m config window_placement second_child
@@ -76,10 +76,6 @@ in
 
         yabai -m signal --add event=dock_did_restart action="sudo yabai --load-sa"
         yabai -m signal --add event=window_focused action="sketchybar --trigger window_focus"
-        # yabai -m signal --add event=display_added action="sleep 2 && $HOME/.config/yabai/create_spaces.sh"
-        # yabai -m signal --add event=display_removed action="sleep 1 && $HOME/.config/yabai/create_spaces.sh"
-        yabai -m signal --add event=window_created action="sketchybar --trigger windows_on_spaces"
-        yabai -m signal --add event=window_destroyed action="sketchybar --trigger windows_on_spaces"
 
         # Exclude problematic apps from being managed:
         yabai -m rule --add app="^(LuLu|Calculator|Software Update|Dictionary|VLC|System Preferences|System Settings|zoom.us|Photo Booth|Archive Utility|Python|LibreOffice|App Store|Steam|Alfred|Activity Monitor|DOSBox)$" manage=off
