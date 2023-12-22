a66208e2dcc80329dee4870ad32dc1cd42e78e68
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2023-12-22 15:32:15 -0800

    open minimum ports

--- modules/services/k3s/nixos.nix
+++ modules/services/k3s/nixos.nix
@@ -14,15 +14,19 @@ in
 {
   config = mkIf cfg.enable {
     networking.firewall.allowedTCPPorts =
-      [ 6443 ] # k3s: required so that pods can reach the API server (running on port 6443 by default)
-      ++ optionals (config.soxincfg.services.k3s.role == "server") [
+      [
+        # 80 # HTTP
+        # 443 # HTTPS
+      ] ++ optionals (config.soxincfg.services.k3s.role == "server") [
+        6443 # k3s: required so that pods can reach the API server (running on port 6443 by default)
         2379 # k3s, etcd clients: required if using a "High Availability Embedded etcd" configuration
         2380 # k3s, etcd peers: required if using a "High Availability Embedded etcd" configuration
       ];
 
     networking.firewall.allowedUDPPorts =
-      optionals (config.soxincfg.services.k3s.role == "server") [
+      [
         8472 # k3s, flannel: required if using multi-node for inter-node networking
+        10250 # k3s, metrics.
       ];
 
     environment.systemPackages = singleton config.services.k3s.package;
