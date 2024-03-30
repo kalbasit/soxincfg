bc27a7d7f018314ccb5ca02e6760d9ac99d89bf1
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2024-03-30 16:58:44 -0700

    hosts/kore: configure mjpg-streamer service

--- hosts/nixoses/kore/nixos.nix
+++ hosts/nixoses/kore/nixos.nix
@@ -8,9 +8,9 @@ in
     soxincfg.nixosModules.profiles.miniserver
 
     ./hardware-configuration.nix
-  ];
 
-  environment.systemPackages = [ pkgs.mjpg-streamer ];
+    ./mjpg-streamer.nix
+  ];
 
   # nixpkgs.config.allowUnfree = true;
   nixpkgs.system = "aarch64-linux";
@@ -24,20 +24,6 @@ in
         "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDKBOvTyKSvp4vENrpyFKkZ+OfBBeSXRvR7CWhPBHbGD19Xev+gAF/D1y5zqdaEK6sZuSmWoBfLHatbfWjiz6Yj1uygYtDilyqSaw0tkdsOMihMHtqvkdNugrzPyfyeugdGUPG50tnXbkTyp8QOfxhkqdpwku0NMLMnMDMOKjGzdYlEFvdPANnjiS2FTrDRbTvb4B64t9OgQ0d/tUpuTMRvRSoTLBtlJC5nNFhNKnhDl6lZDMTQyTZSg8iA25W2C2KQVs5IKJ+E+LMS7golD3t1i/S9kN4guo7yoEU5lQ8xGBqDX5Gnqwl1wriKJP1roIS0tqi9yHCSX18oeyotY7Y3mWg5lIwotgOBYJ3X5IIH1L0oG92aK5dyGedoDUUMZ8GcRX98PqW8WUa0lZRaYyPfmpN5tzhJpKaqwtdKhxgMyESx0UUFTmUwPPgTVvd4gb0P989BguwKKggx591FzGOHVpWwoYcR9S4q+3F+bSzxKFAOet8CARPP2f3v3ULG6pjSycrvy16BMnzIr1kUmlzBQfhFhqa0HR6I9VQu1ND2SOZPz11wTE7zdOWMV68A47tvvemCOM9GSHLATbTeDnZNWwVAICUslkYiiLULTev07bh5OuwQfY+IK0IeCZ4Wsvy52nWz1YsVmLcwPqPCpsi0oqyhEFzBpfhMwATWlaSiQw== Kore Builder"
       ];
       isNormalUser = true;
-    };
-  };
-
-  networking.vlans = {
-    ifcsn0 = {
-      id = 50;
-      interface = "eth0";
-    };
-  };
-
-  networking.interfaces = {
-    ifcsn0 = {
-      useDHCP = true;
-      macAddress = "b8:27:eb:a8:5e:04";
     };
   };
 
