496e6151b98199d145675759ee3027d8a78b7139
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2024-05-19 18:57:17 -0700

    vars/users: update the hashedPassword

--- vars/users/default.nix
+++ vars/users/default.nix
@@ -12,8 +12,8 @@ in
     optionalAttrs (mode == "NixOS")
       {
         # Generate the password with the following command on Linux:
-        # mkpasswd --method=SHA-512 | xargs -I{} sed -e 's:hashedPassword = ".*";:hashedPassword = "{}";:g' -i vars/users/default.nix
-        hashedPassword = "$6$lAOktqm6B9gB0Xff$oSLoR.0Xz2461Y908gYN8tjQlCRnWUK/VgM7k8O7YSN.98RwSGj6FMn9CwYafeQG1uSIldw66f.YOtJSDzSQD0";
+        # mkpasswd --method=SHA-512 | xargs -I{} sed -e 's:hashedPassword = "[^"]\{10,\}";:hashedPassword = "{}";:g' -i vars/users/default.nix
+        hashedPassword = "$6$6jR0WhIP1DmSf67S$qc9of0VxV8GD3dF2DYYohssfU1AB.AJ5EXIVVctAttW.E7Ri6PIgzM3FFE95vr.xfhXxManypBto8fZoJzq8a1";
 
         sshKeys = [
           # YubiKey 5C
