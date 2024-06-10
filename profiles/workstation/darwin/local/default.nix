df27846132ccc162006be04f0e74428cc7e1357d
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2024-06-10 14:00:08 -0700

    profiles/darwin: do not install dbeaver
    I mostly use the brew version to get updates faster

--- profiles/workstation/darwin/local/default.nix
+++ profiles/workstation/darwin/local/default.nix
@@ -89,7 +89,6 @@ in
       # android.enable = true;
       # brave.enable = true;
       # chromium = { enable = true; surfingkeys.enable = true; };
-      dbeaver.enable = true;
       fzf.enable = true;
       git.enable = true;
       mosh.enable = true;
