e492ff02204e3858a837cbe31f04bcee2eb98296
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2024-09-18 21:43:24 -0700

    disable k3s for now, I will add it later

--- hosts/nixoses/zeus/nixos.nix
+++ hosts/nixoses/zeus/nixos.nix
@@ -14,9 +14,9 @@
   services.k3s.extraFlags = builtins.concatStringsSep " " [
     #"--node-label nasreddine.com/has-octoprint-device=yes"
   ];
-  soxincfg.services.k3s = {
-    enable = true;
-    role = "agent";
-    serverAddr = "https://192.168.50.16:6443";
-  };
+  #soxincfg.services.k3s = {
+  #  enable = true;
+  #  role = "agent";
+  #  serverAddr = "https://192.168.50.16:6443";
+  #};
 }
