3d7516eeb6e6e2e1b60ef5784ca7a46cbd382710
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2024-10-22 11:41:34 -0700

    hosts/darwins/poseidon: Update to match Saturn

--- hosts/darwins/poseidon/darwin.nix
+++ hosts/darwins/poseidon/darwin.nix
@@ -14,8 +14,36 @@ in
   # load YL's home-manager configuration
   home-manager.users.yl = import ./home.nix { inherit soxincfg; };
 
-  nix = {
-    useDaemon = true;
+  # Auto-Backup Obsidian Vaults and send them to GitHub
+  launchd.user.agents.auto-backup-obsidian-vaults = {
+    path = [ config.environment.systemPath ];
+
+    serviceConfig.ProgramArguments =
+      let
+        script = pkgs.writeShellScript "auto-backup-obsidian-vaults" ''
+          set -euo pipefail
+
+          if ! test -d ~/SynologyDrive/Obsidian
+          then
+            exit 0
+          fi
+
+          cd ~/SynologyDrive/Obsidian
+
+          while true
+          do
+            git add -A .
+            git commit --no-gpg-sign -m 'Auto-Backup from user agent auto-backup-obsidian-vaults'
+            git push origin main
+
+            sleep 5m
+          done
+        '';
+      in
+      singleton (builtins.toString script);
+
+    serviceConfig.KeepAlive = true;
+    serviceConfig.ProcessType = "Interactive";
   };
 
   system.stateVersion = 5;
