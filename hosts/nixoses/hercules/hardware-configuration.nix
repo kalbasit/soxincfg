38775e8a4a488176c34e260f95eb10c4cf44afed
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2024-11-06 15:01:59 -0800

    hosts/hercules: Sort the imports

--- hosts/nixoses/hercules/hardware-configuration.nix
+++ hosts/nixoses/hercules/hardware-configuration.nix
@@ -13,16 +13,19 @@ let
   windowsDevice = "/dev/disk/by-uuid/28C025C1C025965A";
 
   datasets = {
+    # NixOS
     "/" = { device = "olympus/system/nixos/root"; };
+    "/nix" = { device = "olympus/system/nixos/nix"; };
+    "/var" = { device = "olympus/system/nixos/var"; };
+    "/yl" = { device = "olympus/user/yl/nixos-home"; };
+    "/yl/code" = { device = "olympus/user/yl/code"; };
+
+    # Arch Linux
     "/mnt/arch" = { device = "olympus/system/arch/root"; };
     "/mnt/arch/nix" = { device = "olympus/system/arch/nix"; };
     "/mnt/arch/var" = { device = "olympus/system/arch/var"; };
     "/mnt/arch/yl" = { device = "olympus/user/yl/arch-home"; };
     "/mnt/arch/yl/code" = { device = "olympus/user/yl/code"; };
-    "/nix" = { device = "olympus/system/nixos/nix"; };
-    "/var" = { device = "olympus/system/nixos/var"; };
-    "/yl" = { device = "olympus/user/yl/nixos-home"; };
-    "/yl/code" = { device = "olympus/user/yl/code"; };
   };
 
   mkZFSDataSet = mountPoint: { device }:
