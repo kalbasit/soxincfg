b72609d80cdca297eca4399d02daeddc3a2afb8e
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2024-11-08 22:52:06 -0800

    remove dead code

--- profiles/workstation/qubes/local/home.nix
+++ profiles/workstation/qubes/local/home.nix
@@ -6,13 +6,6 @@ let
     ;
 in
 {
-  # Configure SOPS to use the gpg wrapper
-  # https://github.com/getsops/sops#213specify-a-different-gpg-executable
-  #home.sessionVariables."SOPS_GPG_EXEC" ="qubes-gpg-client";
-  #sops.environment.SOPS_GPG_EXEC ="qubes-gpg-client";
-  #sops.environment.QUBES_GPG_DOMAIN="vault-gpg";
-  #sops.gnupg.home="/dev/null";
-
   sops.gnupg.qubes-split-gpg = { enable = true; domain = "vault-gpg"; };
   sops.secrets.hello = { sopsFile = ./secrets.sops.yaml; };
 
