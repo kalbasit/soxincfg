5b353ba147ed8c2d77c393a349df36f1f00f5686
Wael Nasreddine <wael.nasreddine@gmail.com>
2024-11-08 20:07:54 -0800

    profiles/workstation/qubes: configure sops to use qubes-gpg-client

--- profiles/workstation/qubes/local/home.nix
+++ profiles/workstation/qubes/local/home.nix
@@ -1,3 +1,9 @@
 { ... }:
 
-{ }
+{
+  home.sessionVariables = {
+    # Configure SOPS to use the gpg wrapper
+    # https://github.com/getsops/sops#213specify-a-different-gpg-executable
+    SOPS_GPG_EXEC="qubes-gpg-client";
+  };
+}
