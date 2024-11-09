4e19320e0a2a9b3dee1eb923bffc64150074157a
Wael Nasreddine <wael.nasreddine@gmail.com>
2024-11-08 20:26:38 -0800

    yay working

--- profiles/workstation/qubes/local/home.nix
+++ profiles/workstation/qubes/local/home.nix
@@ -1,9 +1,12 @@
 { ... }:
 
 {
-  home.sessionVariables = {
     # Configure SOPS to use the gpg wrapper
     # https://github.com/getsops/sops#213specify-a-different-gpg-executable
-    SOPS_GPG_EXEC="qubes-gpg-client";
-  };
+  home.sessionVariables."SOPS_GPG_EXEC" ="qubes-gpg-client";
+  sops.environment.SOPS_GPG_EXEC ="qubes-gpg-client";
+  sops.environment.QUBES_GPG_DOMAIN="vault-gpg";
+  sops.gnupg.home="/dev/null";
+
+  sops.secrets.hello = { sopsFile = ./secrets.sops.yaml; };
 }
