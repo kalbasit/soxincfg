bed83d6b869a21ca2ed77435277fe3d81b5320b1
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2024-11-08 21:50:45 -0800

    profiles/workstation/qubes: Enable programs and install binaries
    Mostly copied from profiles/workstation/common but just what I need on
    Qubes.

--- profiles/workstation/qubes/local/home.nix
+++ profiles/workstation/qubes/local/home.nix
@@ -1,3 +1,53 @@
-{ ... }:
+{ config, pkgs,lib, ... }:
 
-{ }
+let
+  inherit (lib)
+  optionals
+  ;
+in {
+  # programs
+  programs.bat.enable = true;
+  programs.direnv.enable = true;
+
+  # files
+  home.file = {
+    ".npmrc".text = "prefix=${config.home.homeDirectory}/.filesystem";
+  };
+
+  home.packages = with pkgs; [
+    amazon-ecr-credential-helper
+    awscli2
+    binutils # for strings
+    devbox
+    docker-credential-gcr
+    duf # du replacement on steroids
+    file
+    fx # JSON viewer
+    gh # GitHub command line client
+    gist
+    gnugrep
+    hexyl # hex viewer with nice colors
+    imagemagick # for convert
+    inetutils # for telnet
+    jq
+    mercurial
+    nix-index
+    nixpkgs-review
+    nix-zsh-completions
+    nur.repos.kalbasit.swm
+    pv # generic progress of data through a pipeline
+    scrcpy # mirror Android device via USB
+    screen # needed to open up terminal devices
+    unzip
+  ] ++ (optionals stdenv.isLinux [
+    #
+    # Linux applications
+    #
+
+    _2048-in-terminal
+    dnsutils # for dig
+    esptool
+    gdb
+    mbuffer # memory buffer within pipeline
+  ]);
+}
