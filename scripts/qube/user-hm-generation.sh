c5f523fbdd65042009e7f49cd2d483935d2ea8eb
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2024-11-08 23:52:28 -0800

    scripts/qube: Activation script should handle broken generation links

--- scripts/qube/user-hm-generation.sh
+++ scripts/qube/user-hm-generation.sh
@@ -2,12 +2,19 @@
 
 if [[ "$(id -u)" -ne "$(id -u user)" ]]
 then
-	echo "You must run this script under the user 'user'" >&2
-	exit 1
+    echo "You must run this script under the user 'user'" >&2
+    exit 1
 fi
 
 if [ -e '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh' ]; then
     . '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'
+fi
+
+if [[ -d /home/user/.local/state/nix/profiles ]] &&
+    [[ "$(find /home/user/.local/state/nix/profiles/ -xtype l | wc -l)" -gt 0 ]]
+then
+    echo "Found dead profile links that will break home-manager activation, going to remove all profiles"
+    rm -rf /home/user/.local/state/nix/profiles
 fi
 
 /user-hm-generation/activate
