66ef97eee6b47d19d40f96aa051f9101670825e0
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2024-11-12 15:43:38 -0800

    profiles/workstation/qubes: Add support for split-ssh

---
+++ profiles/workstation/qubes/local/split-ssh.nix
@@ -0,0 +1,25 @@
+{ config, pkgs, ... }:
+
+{
+  home.file = {
+    ".npmrc".text = "prefix=${config.home.homeDirectory}/.filesystem";
+
+    # xinit will start an ssh-agent overshadowing the agent I created for qubes
+    ".profile".text = "export SSH_AUTH_SOCK=/run/user/$(id -u)/qubes-ssh-agent.socket";
+  };
+
+  systemd.user.services.qubes-ssh-agent = {
+    Unit = {
+      Description = "Split SSH Agent Socket for Vault SSH";
+    };
+
+    Service = {
+      ExecStart = ''
+        ${pkgs.socat}/bin/socat -d -d "UNIX-LISTEN:%t/qubes-ssh-agent.socket,mode=600,fork,unlink-early" "EXEC:qrexec-client-vm vault-ssh qubes.SshAgent"
+      '';
+      Restart = "always";
+    };
+
+    Install = { WantedBy = [ "default.target" ]; };
+  };
+}
