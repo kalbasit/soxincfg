5e12e1e577ddfc9088a0cb9e99539e21fb435394
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2024-11-18 12:06:26 -0800

    profiles/workstation: Update the public key of my GnuPG

--- profiles/workstation/nixos/local/default.nix
+++ profiles/workstation/nixos/local/default.nix
@@ -83,35 +83,34 @@ in
               -----BEGIN PGP PUBLIC KEY BLOCK-----
 
               mDMEYcAFjRYJKwYBBAHaRw8BAQdAc8wiHhA63ylmi83Ee/qRaQQGUJlTongyPmZz
-              CiNse++0JldhZWwgTmFzcmVkZGluZSA8d2FlbEBrZWVwdHJ1Y2tpbi5jb20+iJYE
-              ExYIAD4WIQSMZ6Ant8rDmMiIIFeOvZXcFHdVogUCYcAGBAIbAwUJcMgSAAULCQgH
-              AgYVCgkICwIEFgIDAQIeAQIXgAAKCRCOvZXcFHdVoiqLAQCmSz+RSySWE94gTM3M
-              rXCGQoaxKlI1HcUSGNz+A2jIsgD/Uw4e3UWxu78ROFUl2+8ktcxYUp05F/r8jdR1
-              8V3cLwy0K1dhZWwgTmFzcmVkZGluZSA8d2FlbC5uYXNyZWRkaW5lQGdtYWlsLmNv
-              bT6IlgQTFggAPhYhBIxnoCe3ysOYyIggV469ldwUd1WiBQJhwAWNAhsDBQlwyBIA
-              BQsJCAcCBhUKCQgLAgQWAgMBAh4BAheAAAoJEI69ldwUd1WinnQA/RSwspJK38HD
-              SHzxzK3GQGTgBn48/Z3TXjtFfvELdgGQAQDTvdUKqptO3Dc8yY150c/xMrDZt+/s
-              4Xf87E5g7KL6CLQgV2FlbCBOYXNyZWRkaW5lIDxrYWxiYXNpdEBwbS5tZT6IlgQT
-              FggAPhYhBIxnoCe3ysOYyIggV469ldwUd1WiBQJhwAXaAhsDBQlwyBIABQsJCAcC
-              BhUKCQgLAgQWAgMBAh4BAheAAAoJEI69ldwUd1WiEW4A/3EG071ul1kFV1cDJd5J
-              6U0Yycbe+4z4oJdnEQWOEC9JAQCHlAUJT8jsxdJsFgbq7deyNvmUrALFwh8MoXV6
-              pX39ArQlV2FlbCBOYXNyZWRkaW5lIDx3YWVsQG5hc3JlZGRpbmUuY29tPoiWBBMW
-              CAA+FiEEjGegJ7fKw5jIiCBXjr2V3BR3VaIFAmHABfUCGwMFCXDIEgAFCwkIBwIG
-              FQoJCAsCBBYCAwECHgECF4AACgkQjr2V3BR3VaJqOwD+Oomz/lpVdDd2mK+el2Jn
-              2h4f8qvOeGR1ZIudSdO22UgA/RqF9L91848JKhFOU5h/Ra5r2667R/Yw4R11EhVf
-              fTEKtB5XYWVsIE5hc3JlZGRpbmUgPG1lQGthbGJhcy5pdD6IlgQTFggAPhYhBIxn
-              oCe3ysOYyIggV469ldwUd1WiBQJhwAYsAhsDBQlwyBIABQsJCAcCBhUKCQgLAgQW
-              AgMBAh4BAheAAAoJEI69ldwUd1Wij64A/20VmcipGdugNeX9+FfwNhJc2eAhtbTA
-              to0Mv4bGIwmlAQC7GKuXBlZFZBTC6YWK74yHL+Q24I+WpjkJqP0Ws2RzA7QdV2Fl
-              bCBOYXNyZWRkaW5lIDxtZUB5bC5jb2Rlcz6IlgQTFggAPhYhBIxnoCe3ysOYyIgg
-              V469ldwUd1WiBQJhwAY5AhsDBQlwyBIABQsJCAcCBhUKCQgLAgQWAgMBAh4BAheA
-              AAoJEI69ldwUd1WiPesA/0ltEdlFYsgBlond2/9DOFzJbZMpptE6hUkgQDKkfzK5
-              AQD6cGm2vepyhRUSbhi/3IOU964ZDBIU4ca6Qn/cS6YhAbg4BGHABY0SCisGAQQB
-              l1UBBQEBB0CJBE4hQa+D4ByAWV5SmQdJ5eEg1OE84c4FPOZ38uGBAgMBCAeIfgQY
-              FggAJhYhBIxnoCe3ysOYyIggV469ldwUd1WiBQJhwAWNAhsMBQlwyBIAAAoJEI69
-              ldwUd1WivCMA+QE5L/tBDmQPeRjziccWGTaVx2IL586VBFhJoFHe/0wNAQC3so45
-              pmbSq/gNkA/X7zdwtT4aVMbFzOR2nZ28X/gnCw==
-              =IvCC
+              CiNse++0K1dhZWwgTmFzcmVkZGluZSA8d2FlbC5uYXNyZWRkaW5lQGdtYWlsLmNv
+              bT6ImQQTFggAQQUJcMgSAAULCQgHAgYVCgkICwIEFgIDAQIeAQIXgAIZARYhBIxn
+              oCe3ysOYyIggV469ldwUd1WiBQJnOtcKAhsBAAoJEI69ldwUd1WiujAA/0irnKnN
+              bVcuS98jd1fGJQGnRqo5YWh1P277Gk9hMWH7AP9sgZRnzrML1ypbjJ8U+otT7Ptd
+              8r1dKXrTj53iLtDvCbQgV2FlbCBOYXNyZWRkaW5lIDxrYWxiYXNpdEBwbS5tZT6I
+              lgQTFggAPhYhBIxnoCe3ysOYyIggV469ldwUd1WiBQJhwAXaAhsDBQlwyBIABQsJ
+              CAcCBhUKCQgLAgQWAgMBAh4BAheAAAoJEI69ldwUd1WiEW4A/3EG071ul1kFV1cD
+              Jd5J6U0Yycbe+4z4oJdnEQWOEC9JAQCHlAUJT8jsxdJsFgbq7deyNvmUrALFwh8M
+              oXV6pX39ArQlV2FlbCBOYXNyZWRkaW5lIDx3YWVsQG5hc3JlZGRpbmUuY29tPoiW
+              BBMWCAA+FiEEjGegJ7fKw5jIiCBXjr2V3BR3VaIFAmHABfUCGwMFCXDIEgAFCwkI
+              BwIGFQoJCAsCBBYCAwECHgECF4AACgkQjr2V3BR3VaJqOwD+Oomz/lpVdDd2mK+e
+              l2Jn2h4f8qvOeGR1ZIudSdO22UgA/RqF9L91848JKhFOU5h/Ra5r2667R/Yw4R11
+              EhVffTEKtB5XYWVsIE5hc3JlZGRpbmUgPG1lQGthbGJhcy5pdD6IlgQTFggAPhYh
+              BIxnoCe3ysOYyIggV469ldwUd1WiBQJhwAYsAhsDBQlwyBIABQsJCAcCBhUKCQgL
+              AgQWAgMBAh4BAheAAAoJEI69ldwUd1Wij64A/20VmcipGdugNeX9+FfwNhJc2eAh
+              tbTAto0Mv4bGIwmlAQC7GKuXBlZFZBTC6YWK74yHL+Q24I+WpjkJqP0Ws2RzA7g4
+              BGHABY0SCisGAQQBl1UBBQEBB0CJBE4hQa+D4ByAWV5SmQdJ5eEg1OE84c4FPOZ3
+              8uGBAgMBCAeIfgQYFggAJhYhBIxnoCe3ysOYyIggV469ldwUd1WiBQJhwAWNAhsM
+              BQlwyBIAAAoJEI69ldwUd1WivCMA+QE5L/tBDmQPeRjziccWGTaVx2IL586VBFhJ
+              oFHe/0wNAQC3so45pmbSq/gNkA/X7zdwtT4aVMbFzOR2nZ28X/gnC7gzBGc61w8W
+              CSsGAQQB2kcPAQEHQIUfWeLXQlYun0/WmzAsBbDT6TeWD/SCFD5Fes9I5mJQiPUE
+              GBYKACYWIQSMZ6Ant8rDmMiIIFeOvZXcFHdVogUCZzrXDwIbAgUJaUNEAACBCRCO
+              vZXcFHdVonYgBBkWCgAdFiEEiP6mvZ+9ob6ykU7k7px+1rpJsc4FAmc61w8ACgkQ
+              7px+1rpJsc6olwD7BZ9eMC5cnmM6R4c4Mizo0MOtD5A+2+ZQagNiAhS2MHMBAI0Q
+              C1buJtQh+ob/5H+VyNiRTW3tSurSR9eEdGitPBYAZbEA/2b4atE/GurIlbQf2FWi
+              SjIL1ZIcAmypXQfXL0Klc5H8AQCtkML4KiIFuwtO1Rdfg9ToR9Etv4RKHkc9dP4m
+              iT+JAg==
+              =i8JY
               -----END PGP PUBLIC KEY BLOCK-----
             '';
             decryption-key-slot = 101;
