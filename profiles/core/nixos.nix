2db109968c88bd7454698feee118b28119668e0e
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2024-12-03 20:10:35 -0800

    profiles/core: add more substituers

--- profiles/core/nixos.nix
+++ profiles/core/nixos.nix
@@ -12,16 +12,33 @@ let
   users = soxincfg.vars.users { inherit lib mode; };
 in
 {
-  # enable the Nix sandbox
-  nix.settings.sandbox = true;
+  nix = {
+    settings = {
+      # enable the Nix sandbox
+      sandbox = true;
 
-  # setup NIX_PATH to allow users to access the nixpkgs that built the system
-  nix.nixPath = [
-    "nixpkgs-unstable=${inputs.nixpkgs-unstable}"
-    "nixpkgs=${pkgs.path}"
-    "soxin=${soxin}"
-    "soxincfg=${soxincfg}"
-  ];
+      substituters = [
+        # add my home cache to the list of substituters
+        "https://nix-cache.cluster.ifcsn0.nasreddine.com"
+
+        # add nix-community maintained cache
+        "https://nix-community.cachix.org"
+      ];
+
+      trusted-public-keys = [
+        "nix-cache.cluster.nasreddine.com:gAaW+smlYAvg/u94P1XmbI45aIhJ9/5upB5QrKY33B0="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      ];
+    };
+
+    # setup NIX_PATH to allow users to access the nixpkgs that built the system
+    nixPath = [
+      "nixpkgs-unstable=${inputs.nixpkgs-unstable}"
+      "nixpkgs=${pkgs.path}"
+      "soxin=${soxin}"
+      "soxincfg=${soxincfg}"
+    ];
+  };
 
   boot.tmp.useTmpfs = true;
 
