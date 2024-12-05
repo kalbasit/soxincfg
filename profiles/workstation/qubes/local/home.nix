24e5354bb73abc2b21ef95fe1d80f0d4e6d4e9ed
Wael Nasreddine <wael.nasreddine@gmail.com>
2024-12-05 15:43:08 -0800

    profiles/workstation/qubes: remove the isLinux conditional, it is only Linux

--- profiles/workstation/qubes/local/home.nix
+++ profiles/workstation/qubes/local/home.nix
@@ -29,45 +29,37 @@ in
       ".npmrc".text = "prefix=${config.home.homeDirectory}/.filesystem";
     };
 
-    home.packages =
-      with pkgs;
-      [
-        amazon-ecr-credential-helper
-        awscli2
-        binutils # for strings
-        bitwarden-cli
-        devbox
-        docker-credential-gcr
-        duf # du replacement on steroids
-        file
-        fx # JSON viewer
-        gh # GitHub command line client
-        gist
-        gnugrep
-        hexyl # hex viewer with nice colors
-        imagemagick # for convert
-        inetutils # for telnet
-        jq
-        mercurial
-        nix-index
-        nixpkgs-review
-        nix-zsh-completions
-        nur.repos.kalbasit.swm
-        pv # generic progress of data through a pipeline
-        scrcpy # mirror Android device via USB
-        screen # needed to open up terminal devices
-        unzip
-      ]
-      ++ (optionals stdenv.isLinux [
-        #
-        # Linux applications
-        #
-
-        _2048-in-terminal
-        dnsutils # for dig
-        esptool
-        gdb
-        mbuffer # memory buffer within pipeline
-      ]);
+    home.packages = with pkgs; [
+      _2048-in-terminal
+      amazon-ecr-credential-helper
+      awscli2
+      binutils # for strings
+      bitwarden-cli
+      devbox
+      dnsutils # for dig
+      docker-credential-gcr
+      duf # du replacement on steroids
+      esptool
+      file
+      fx # JSON viewer
+      gdb
+      gh # GitHub command line client
+      gist
+      gnugrep
+      hexyl # hex viewer with nice colors
+      imagemagick # for convert
+      inetutils # for telnet
+      jq
+      mbuffer # memory buffer within pipeline
+      mercurial
+      nix-index
+      nixpkgs-review
+      nix-zsh-completions
+      nur.repos.kalbasit.swm
+      pv # generic progress of data through a pipeline
+      scrcpy # mirror Android device via USB
+      screen # needed to open up terminal devices
+      unzip
+    ];
   };
 }
