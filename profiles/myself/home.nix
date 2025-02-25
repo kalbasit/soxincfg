7bff296aecc9d983d4991ef35d534463f5b72ba0
Wael Nasreddine <wael.nasreddine@gmail.com>
2025-02-24 19:32:53 -0800

    profiles/myself: move private ssh stuff to config myself

--- profiles/myself/home.nix
+++ profiles/myself/home.nix
@@ -40,59 +40,6 @@ in
       extraConfig = ''
         Include ~/.ssh/config_include_myself
       '';
-
-      matchBlocks = {
-        "bitbucket.org" = {
-          extraOptions = {
-            IdentityFile = "~/.ssh/per-host/bitbucket.org_rsa";
-          };
-        };
-
-        "gitlab.com" = {
-          extraOptions = {
-            IdentityFile = "~/.ssh/per-host/gitlab.com_rsa";
-          };
-        };
-
-        "serial" = {
-          extraOptions = {
-            IdentityFile = "~/.ssh/per-host/serial.nasreddine.com_rsa";
-          };
-        };
-
-        "unifi-usg" = {
-          hostname = "192.168.10.1";
-          extraOptions = {
-            IdentityFile = "~/.ssh/per-host/unifi.nasreddine.com_rsa";
-            # Unifi is still using hmac-sha1, enabling it only for these hosts.
-            MACs = "+hmac-sha1";
-            PubkeyAcceptedKeyTypes = "+ssh-rsa";
-          };
-          user = "admin";
-        };
-
-        "unifi-switch" = {
-          hostname = "192.168.10.44";
-          extraOptions = {
-            IdentityFile = "~/.ssh/per-host/unifi.nasreddine.com_rsa";
-            # Unifi is still using hmac-sha1, enabling it only for these hosts.
-            MACs = "+hmac-sha1";
-            PubkeyAcceptedKeyTypes = "+ssh-rsa";
-          };
-          user = "admin";
-        };
-
-        "unifi-ap0" = {
-          hostname = "192.168.10.218";
-          extraOptions = {
-            IdentityFile = "~/.ssh/per-host/unifi.nasreddine.com_rsa";
-            # Unifi is still using hmac-sha1, enabling it only for these hosts.
-            MACs = "+hmac-sha1";
-            PubkeyAcceptedKeyTypes = "+ssh-rsa";
-          };
-          user = "admin";
-        };
-      };
     };
   };
 }
