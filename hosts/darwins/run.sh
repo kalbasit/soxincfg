e0543c9503cc54f48b9ff9d16c6042b6cbe79e80
Wael Nasreddine <wael.nasreddine@gmail.com>
2025-04-10 00:08:23 -0700

    hosts/darwins/run.sh: new script to deal with building/switching darwins

---
+++ hosts/darwins/run.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+readonly action="${1:-}"
+readonly host="${2:-$(hostname)}"
+
+if type nom &> /dev/null
+then
+  BUILDER=nom
+else
+  BUILDER=nix
+fi
+
+readonly BUILDER
+
+case "${action}" in
+    build)
+        $BUILDER build ".#darwinConfigurations.${host}.system"
+        ;;
+    switch)
+        >&2 echo "Switching $host"
+
+        # steps taken from https://github.com/LnL7/nix-darwin/blob/8b6ea26d5d2e8359d06278364f41fbc4b903b28a/pkgs/nix-tools/darwin-rebuild.sh
+
+        # 1. build the host
+        "$0" build "$host"
+
+        # 2. setup the profile
+        profile=/nix/var/nix/profiles/system
+        systemConfig="$(readlink -f result)"
+        sudo -H nix-env -p "$profile" --set "$systemConfig"
+
+        # 3. call darwin-rebuild activate
+        ./result/sw/bin/darwin-rebuild activate
+        ;;
+    *)
+        usage
+        exit 1
+        ;;
+esac
--- scripts/host.sh
+++ scripts/host.sh
@@ -7,11 +7,6 @@ readonly host="${2:-$(hostname)}"
 
 readonly root_dir="$(cd $(dirname "$BASH_SOURCE[0]")/.. && pwd)"
 
-isDarwin() {
-	local os="$(uname -s)"
-	[[ "${os}" == "Darwin" ]]
-}
-
 isLinux() {
 	local os="$(uname -s)"
 	[[ "${os}" == "Linux" ]]
@@ -38,7 +33,6 @@ has() {
 
 isSupported() {
     isNixOS && return 0
-    isDarwin && return 0
 
     if [[ -f /etc/os-release ]]; then
         local os="$(awk -F= '/^ID=/ {print $2}' /etc/os-release | tr -d '\n')"
@@ -74,8 +68,6 @@ case "${action}" in
         >&2 echo "Building $host"
         if isNixOS; then
             nom build ".#nixosConfigurations.${host}.config.system.build.toplevel"
-        elif isDarwin; then
-            nom build ".#darwinConfigurations.${host}.system" --show-trace
         else
             nom build ".#homeConfigurations.${host}.activationPackage"
         fi
@@ -84,9 +76,6 @@ case "${action}" in
         >&2 echo "Testing $host"
         if isNixOS; then
             nixos-rebuild --use-remote-sudo --flake ".#${host}" test # --show-trace
-        elif isDarwin; then
-            >&2 echo test is not support on nix-darwin
-            exit 1
         else
             >&2 echo test is not support on home-manager
             exit 1
@@ -96,19 +85,6 @@ case "${action}" in
         >&2 echo "Switching $host"
         if isNixOS; then
             nixos-rebuild --use-remote-sudo --flake ".#${host}" switch # --show-trace
-        elif isDarwin; then
-            # steps taken from https://github.com/LnL7/nix-darwin/blob/8b6ea26d5d2e8359d06278364f41fbc4b903b28a/pkgs/nix-tools/darwin-rebuild.sh
-
-            # 1. build the host
-            "$0" build "$host"
-
-            # 2. setup the profile
-            profile=/nix/var/nix/profiles/system
-            systemConfig="$(readlink -f result)"
-            sudo -H nix-env -p "$profile" --set "$systemConfig"
-
-            # 3. call darwin-rebuild activate
-            ./result/sw/bin/darwin-rebuild activate
         else
             home-manager switch --flake ".#${host}"
             $(nix path-info ".#homeConfigurations.${host}.activationPackage")/activate
@@ -118,9 +94,6 @@ case "${action}" in
         >&2 echo "Booting $host"
         if isNixOS; then
             nixos-rebuild --use-remote-sudo --flake ".#${host}" boot # --show-trace
-        elif isDarwin; then
-            >&2 echo boot is not support on nix-darwin
-            exit 1
         else
             >&2 echo boot is not support on home-manager
             exit 1
