023b23a42966c0b9bba65555f519f1350f6d84f9
Wael Nasreddine <wael.nasreddine@gmail.com>
2025-04-11 11:56:50 -0700

    services.skhd: restore nix-darwin driven configuration

--- modules/services/skhd/darwin.nix
+++ modules/services/skhd/darwin.nix
@@ -1,15 +1,50 @@
-{ config, lib, ... }:
+{
+  config,
+  lib,
+  ...
+}:
 
 let
-  inherit (lib) mkIf;
+  inherit (lib)
+    mkIf
+    ;
 
   cfg = config.soxincfg.services.skhd;
 in
 {
   config = mkIf cfg.enable {
-    homebrew = {
-      brews = [ "koekeishiya/formulae/skhd" ];
-      taps = [ "koekeishiya/formulae" ];
+    services.skhd = {
+      enable = true;
+      skhdConfig = ''
+        # focus window
+        alt - n : yabai -m window --focus west || yabai -m display --focus west
+        alt - e : yabai -m window --focus south || yabai -m display --focus south
+        alt - i : yabai -m window --focus north || yabai -m display --focus north
+        alt - o : yabai -m window --focus east || yabai -m display --focus east
+
+        # Float / Unfloat window: lalt - space
+        alt - space : yabai -m window --toggle float; sketchybar --trigger window_focus
+
+        # swap window
+        shift + alt - n : yabai -m window --warp west || $(yabai -m window --display west && yabai -m display --focus west && yabai -m window --warp last) || yabai -m window --move rel:-10:0
+        shift + alt - e : yabai -m window --warp south || $(yabai -m window --display south && yabai -m display --focus south) || yabai -m window --move rel:0:10
+        shift + alt - i : yabai -m window --warp north || $(yabai -m window --display north && yabai -m display --focus north) || yabai -m window --move rel:0:-10
+        shift + alt - o : yabai -m window --warp east || $(yabai -m window --display east && yabai -m display --focus east && yabai -m window --warp first) || yabai -m window --move rel:10:0
+
+        # toggle window zoom
+        alt - f : yabai -m window --toggle zoom-parent; sketchybar --trigger window_focus
+        shift + alt - f : yabai -m window --toggle zoom-fullscreen; sketchybar --trigger window_focus
+
+        # Equalize size of windows: ctrl + alt - e
+        ctrl + alt - e : yabai -m space --balance
+
+        # Insertion (shift + ctrl + alt - ...)
+        # Set insertion point for focused container: shift + ctrl + alt - {n, e, i, o}
+        shift + ctrl + lalt - n : yabai -m window --insert west
+        shift + ctrl + lalt - e : yabai -m window --insert south
+        shift + ctrl + lalt - i : yabai -m window --insert north
+        shift + ctrl + lalt - o : yabai -m window --insert east
+      '';
     };
   };
 }
--- modules/services/skhd/default.nix
+++ modules/services/skhd/default.nix
@@ -1,12 +1,13 @@
 { lib, mode, ... }:
 
 let
-  inherit (lib) mkEnableOption optionals;
+  inherit (lib)
+    mkEnableOption
+    optionals
+    ;
 in
 {
-  imports =
-    optionals (mode == "home-manager") [ ./home-manager.nix ]
-    ++ optionals (mode == "nix-darwin") [ ./darwin.nix ];
+  imports = optionals (mode == "nix-darwin") [ ./darwin.nix ];
 
   options.soxincfg.services.skhd.enable = mkEnableOption "Install and configure Skhd";
 }
