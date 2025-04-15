4880c3d0f82228681b1e2c71a50d9f231920ec41
Wael Nasreddine <wael.nasreddine@gmail.com>
2025-04-15 11:24:29 -0700

    hosts/nixoses/run.sh: create a new script for nixoses

---
+++ hosts/nixoses/run.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+readonly action="${1:-}"
+readonly host="${2:-$(hostname)}"
+
+if type nom &> /dev/null
+then
+  BUILDER=nom
+else
+  BUILDER=nix
+fi
+
+readonly BUILDER
+
+case "${action}" in
+  boot)
+    >&2 echo "Booting $host"
+
+    nixos-rebuild --use-remote-sudo --flake ".#${host}" boot --show-trace
+    ;;
+  build)
+    >&2 echo "Building $host"
+
+    $BUILDER build ".#nixosConfigurations.${host}.config.system.build.toplevel" --show-trace
+    ;;
+  switch)
+    >&2 echo "Switching $host"
+
+    nixos-rebuild --use-remote-sudo --flake ".#${host}" switch --show-trace
+    ;;
+  test)
+    >&2 echo "Testing $host"
+
+    nixos-rebuild --use-remote-sudo --flake ".#${host}" test --show-trace
+    ;;
+  *)
+    usage
+    exit 1
+    ;;
+esac
