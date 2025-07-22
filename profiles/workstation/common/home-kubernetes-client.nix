28659a28b0c5ae7f48ce0548d7dcb37bc9a29501
Wael Nasreddine <wael.nasreddine@gmail.com>
2025-07-22 10:19:53 -0700

    feat(kubernetes-client): Put back kc with a warning

--- profiles/workstation/common/home-kubernetes-client.nix
+++ profiles/workstation/common/home-kubernetes-client.nix
@@ -20,6 +20,29 @@
     (pkgs.writeShellScriptBin "k" ''
       ${pkgs.kubecolor}/bin/kubecolor "$@"
     '')
+    (pkgs.writeShellScriptBin "kc" ''
+      BG_RED="\033[41m"
+      FG_BLACK_B="\033[1;30m"
+      FG_CLEAR="\033[0m"
+      readonly BG_RED FG_BLACK_B FG_CLEAR
+
+      state_file="$HOME/.local/state/kc-call-count"
+      readonly state_file
+
+      if [[ ! -f "$state_file" ]]; then
+        mkdir -p "$(dirname "$state_file")"
+        echo -n 0 > "$state_file"
+      fi
+
+      count="$(tr -d '\n' < "$state_file")"
+      count=$(( count + 1 ))
+
+      echo -n "$count" > "$state_file"
+
+      echo -e "$BG_RED$FG_BLACK_B !WARNING!$FG_CLEAR Reminder to use k instead of kc. You made this mistake $count times already"
+
+      ${pkgs.kubecolor}/bin/kubecolor "$@"
+    '')
 
     pkgs.kubectx
     (pkgs.writeShellScriptBin "kcc" ''
