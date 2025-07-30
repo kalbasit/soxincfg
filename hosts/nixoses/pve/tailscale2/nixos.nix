eab3f8273673c50b1ec05701e913e8ac24356332
Wael Nasreddine <wael.nasreddine@gmail.com>
2025-07-29 20:48:59 -0700

    feat(pve-tailscale2): Configured DNSMasq to handle requests for my home DNS

--- hosts/nixoses/pve/tailscale2/nixos.nix
+++ hosts/nixoses/pve/tailscale2/nixos.nix
@@ -1,5 +1,3 @@
-{ _ }:
-
 let
   advertiseRoutes = [
     "192.168.10.0/24"
@@ -18,6 +16,29 @@ in
   imports = [
     ../nixos-25.05/nixos.nix
   ];
+
+  networking.firewall.allowedTCPPorts = [ 53 ];
+  networking.firewall.allowedUDPPorts = [ 53 ];
+
+  services.dnsmasq = {
+    enable = true;
+
+    resolveLocalQueries = false;
+
+    settings = {
+      # Listen on the local LAN interface.
+      # listen-address=127.0.0.1,192.168.100.7
+
+      # Forward ALL queries to Tailscale's resolver. It will handle the rest.
+      server = [ "100.100.100.100" ];
+
+      # Do not read /etc/resolv.conf.
+      no-resolv = true;
+
+      # Cache up to n entries
+      cache-size = "10000";
+    };
+  };
 
   services.tailscale = {
     disableTaildrop = true;
