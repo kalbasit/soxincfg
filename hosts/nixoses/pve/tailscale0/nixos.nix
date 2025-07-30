dbe0975064f010829e8d14d7a41c335c96693b16
Wael Nasreddine <wael.nasreddine@gmail.com>
2025-07-29 23:06:45 -0700

    feat(hosts/pve-tailscale0): Configure NAT so the host traffic to the TailNET

--- hosts/nixoses/pve/tailscale0/nixos.nix
+++ hosts/nixoses/pve/tailscale0/nixos.nix
@@ -6,4 +6,13 @@
 
     ../nixos-25.05/nixos.nix
   ];
+
+  # Configure Network Address Translation (NAT) to act as static route for 100.64.0.0/10
+  networking.nat = {
+    enable = true;
+    # Traffic from internalInterfaces will be NAT'd to the externalInterface.
+    externalInterface = "tailscale0";
+    # Replace 'ens18' with your actual LAN interface name.
+    internalInterfaces = [ "ens18" ];
+  };
 }
