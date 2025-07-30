2e1f6f372b39d279627960567ca4afd9b8a3a9de
Wael Nasreddine <wael.nasreddine@gmail.com>
2025-07-30 11:56:12 -0700

    fix(pve/dns): Configure unicastPeers to properly elect the master

--- hosts/nixoses/pve/dns0/nixos.nix
+++ hosts/nixoses/pve/dns0/nixos.nix
@@ -15,6 +15,11 @@
   services.keepalived.vrrpInstances.dns0 = {
     interface = "ens18";
     priority = 103;
+    unicastPeers = [
+      # "192.168.20.2" # this machine
+      "192.168.20.3"
+      "192.168.20.4"
+    ];
     virtualRouterId = 20;
     virtualIps = [
       { addr = "192.168.20.20/24"; }
--- hosts/nixoses/pve/dns2/nixos.nix
+++ hosts/nixoses/pve/dns2/nixos.nix
@@ -15,6 +15,11 @@
   services.keepalived.vrrpInstances.dns2 = {
     interface = "ens18";
     priority = 100;
+    unicastPeers = [
+      "192.168.20.2"
+      "192.168.20.3"
+      # "192.168.20.4" # this machine
+    ];
     virtualRouterId = 20;
     virtualIps = [
       { addr = "192.168.20.20/24"; }
