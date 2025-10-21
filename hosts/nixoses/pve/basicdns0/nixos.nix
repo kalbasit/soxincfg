c25989147cea86047b73571e65fca59d9270c9c7
Wael Nasreddine <wael.nasreddine@gmail.com>
2025-10-20 22:58:18 -0700

    feat(hosts/basicdns0): Set the static IP

--- hosts/nixoses/pve/basicdns0/nixos.nix
+++ hosts/nixoses/pve/basicdns0/nixos.nix
@@ -10,6 +10,20 @@
     ../nixos-25.05/nixos.nix
   ];
 
+  networking = {
+    defaultGateway = {
+      address = "192.168.20.1";
+      interface = "ens18";
+    };
+
+    interfaces.ens18.ipv4.addresses = [
+      {
+        address = "192.168.20.10";
+        prefixLength = 24;
+      }
+    ];
+  };
+
   services.keepalived.vrrpInstances.dns_vip = {
     priority = 105;
     unicastPeers = [
