ec4e9539c3b24d0d2310e696c0a3a1bcef6e3ea4
Wael Nasreddine <wael.nasreddine@gmail.com>
2025-10-22 15:03:36 -0700

    feat(pve-tailscale0): Set the static IP

--- hosts/nixoses/pve/tailscale0/nixos.nix
+++ hosts/nixoses/pve/tailscale0/nixos.nix
@@ -7,6 +7,24 @@
     ../nixos-25.05/nixos.nix
   ];
 
+  fileSystems."/".device = "/dev/disk/by-uuid/4ff2bf09-2972-4b83-b87f-aa6379c9b3f7";
+
+  fileSystems."/boot".device = "/dev/disk/by-uuid/4329-FF5A";
+
+  networking = {
+    defaultGateway = {
+      address = "192.168.100.1";
+      interface = "ens18";
+    };
+
+    interfaces.ens18.ipv4.addresses = [
+      {
+        address = "192.168.100.10";
+        prefixLength = 24;
+      }
+    ];
+  };
+
   # Configure Network Address Translation (NAT) to act as static route for 100.64.0.0/10
   networking.nat = {
     enable = true;
