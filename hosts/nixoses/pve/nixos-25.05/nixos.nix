16c926be1936ba10c5fe65738256ff35dbd55765
Wael Nasreddine <wael.nasreddine@gmail.com>
2025-10-22 17:46:24 -0700

    fix(pve-nixos-25.05): Turn off DHCP

--- hosts/nixoses/pve/nixos-25.05/nixos.nix
+++ hosts/nixoses/pve/nixos-25.05/nixos.nix
@@ -10,10 +10,14 @@
     ./hardware-configuration.nix
   ];
 
-  networking.nameservers = [
-    "8.8.8.8"
-    "8.8.4.4"
-  ];
+  networking = {
+    nameservers = [
+      "8.8.8.8"
+      "8.8.4.4"
+    ];
+
+    useDHCP = false;
+  };
 
   system.stateVersion = "25.05";
 }
