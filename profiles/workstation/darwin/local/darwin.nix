ca0337f6ea6d8318f1508d1c20a346f6a570cc43
Wael Nasreddine <wael.nasreddine@gmail.com>
2026-01-22 12:04:05 -0800

    fix: set values with default priority

--- profiles/workstation/darwin/local/darwin.nix
+++ profiles/workstation/darwin/local/darwin.nix
@@ -1,6 +1,7 @@
 {
   config,
   pkgs,
+  lib,
   ...
 }:
 
@@ -36,200 +37,200 @@
       defaults = {
         dock = {
           # Enable highlight hover effect for the grid view of a stack (Dock)
-          mouse-over-hilite-stack = true;
+          mouse-over-hilite-stack = lib.mkDefault true;
 
           # Set the icon size of Dock items to 36 pixels
-          tilesize = 36;
+          tilesize = lib.mkDefault 36;
 
           # Change minimize/maximize window effect
-          mineffect = "scale";
+          mineffect = lib.mkDefault "scale";
 
           # Minimize windows into their application’s icon
-          minimize-to-application = true;
+          minimize-to-application = lib.mkDefault true;
 
           # Enable spring loading for all Dock items
-          enable-spring-load-actions-on-all-items = true;
+          enable-spring-load-actions-on-all-items = lib.mkDefault true;
 
           # Show indicator lights for open applications in the Dock
-          show-process-indicators = true;
+          show-process-indicators = lib.mkDefault true;
 
           # Don’t animate opening applications from the Dock
-          launchanim = false;
+          launchanim = lib.mkDefault false;
 
           # Speed up Mission Control animations
-          expose-animation-duration = 0.1;
+          expose-animation-duration = lib.mkDefault 0.1;
 
           # Don’t group windows by application in Mission Control
           # (i.e. use the old Exposé behavior instead)
-          expose-group-apps = false;
+          expose-group-apps = lib.mkDefault false;
 
           # Don’t show Dashboard as a Space
-          dashboard-in-overlay = true;
+          dashboard-in-overlay = lib.mkDefault true;
 
           # Don’t automatically rearrange Spaces based on most recent use
-          mru-spaces = false;
+          mru-spaces = lib.mkDefault false;
 
           # Remove the auto-hiding Dock delay
-          autohide-delay = 0.0;
+          autohide-delay = lib.mkDefault 0.0;
           # Remove the animation when hiding/showing the Dock
-          autohide-time-modifier = 0.0;
+          autohide-time-modifier = lib.mkDefault 0.0;
 
           # Automatically hide and show the Dock
-          autohide = true;
+          autohide = lib.mkDefault true;
 
           # Make Dock icons of hidden applications translucent
-          showhidden = true;
+          showhidden = lib.mkDefault true;
 
           # Don’t show recent applications in Dock
-          show-recents = false;
+          show-recents = lib.mkDefault false;
 
           # disable all hot corners
-          wvous-bl-corner = 1;
-          wvous-br-corner = 1;
-          wvous-tl-corner = 1;
-          wvous-tr-corner = 1;
+          wvous-bl-corner = lib.mkDefault 1;
+          wvous-br-corner = lib.mkDefault 1;
+          wvous-tl-corner = lib.mkDefault 1;
+          wvous-tr-corner = lib.mkDefault 1;
         };
 
         finder = {
           # Finder: allow quitting via ⌘ + Q; doing so will also hide desktop icons
-          QuitMenuItem = true;
+          QuitMenuItem = lib.mkDefault true;
 
           # Display full POSIX path as Finder window title
-          _FXShowPosixPathInTitle = true;
+          _FXShowPosixPathInTitle = lib.mkDefault true;
 
           # Disable the warning when changing a file extension
-          FXEnableExtensionChangeWarning = false;
+          FXEnableExtensionChangeWarning = lib.mkDefault false;
 
           # Do not show any items on the Desktop
-          ShowExternalHardDrivesOnDesktop = false;
-          ShowHardDrivesOnDesktop = false;
-          ShowRemovableMediaOnDesktop = false;
-          ShowMountedServersOnDesktop = false;
+          ShowExternalHardDrivesOnDesktop = lib.mkDefault false;
+          ShowHardDrivesOnDesktop = lib.mkDefault false;
+          ShowRemovableMediaOnDesktop = lib.mkDefault false;
+          ShowMountedServersOnDesktop = lib.mkDefault false;
 
           # Show path breadcrumbs on path bar
-          ShowPathbar = true;
+          ShowPathbar = lib.mkDefault true;
 
           # Show status bar at the bottom of finder windows
-          ShowStatusBar = true;
+          ShowStatusBar = lib.mkDefault true;
 
           # Keep folders on top when sorting by name
-          _FXSortFoldersFirst = true;
+          _FXSortFoldersFirst = lib.mkDefault true;
 
           # Default the current search scope to current folder not this mac
-          FXDefaultSearchScope = "SCcf";
+          FXDefaultSearchScope = lib.mkDefault "SCcf";
         };
 
         # Disable the “Are you sure you want to open this application?” dialog
-        LaunchServices.LSQuarantine = false;
+        LaunchServices.LSQuarantine = lib.mkDefault false;
 
         # Show the clock in 24 hours format
-        menuExtraClock.Show24Hour = true;
+        menuExtraClock.Show24Hour = lib.mkDefault true;
 
         NSGlobalDomain = {
           # Enable “natural” (Lion-style) scrolling (mths.be/macos disables it)
-          "com.apple.swipescrolldirection" = true;
+          "com.apple.swipescrolldirection" = lib.mkDefault true;
 
           # Set sidebar icon size to medium
-          NSTableViewDefaultSizeMode = 2;
+          NSTableViewDefaultSizeMode = lib.mkDefault 2;
 
           # Force clock to be 24-hours
-          AppleICUForce24HourTime = true;
+          AppleICUForce24HourTime = lib.mkDefault true;
 
           # Always show scrollbars
-          AppleShowScrollBars = "Always";
+          AppleShowScrollBars = lib.mkDefault "Always";
 
           # Metric everywhere
-          AppleMeasurementUnits = "Centimeters";
-          AppleMetricUnits = 1;
-          AppleTemperatureUnit = "Celsius";
+          AppleMeasurementUnits = lib.mkDefault "Centimeters";
+          AppleMetricUnits = lib.mkDefault 1;
+          AppleTemperatureUnit = lib.mkDefault "Celsius";
 
           # Disable the over-the-top focus ring animation
-          NSUseAnimatedFocusRing = false;
+          NSUseAnimatedFocusRing = lib.mkDefault false;
 
           # Increase window resize speed for Cocoa applications
-          NSWindowResizeTime = 1.0e-3;
+          NSWindowResizeTime = lib.mkDefault 1.0e-3;
 
           # Expand save panel by default
-          NSNavPanelExpandedStateForSaveMode = true;
-          NSNavPanelExpandedStateForSaveMode2 = true;
+          NSNavPanelExpandedStateForSaveMode = lib.mkDefault true;
+          NSNavPanelExpandedStateForSaveMode2 = lib.mkDefault true;
 
           # Expand print panel by default
-          PMPrintingExpandedStateForPrint = true;
-          PMPrintingExpandedStateForPrint2 = true;
+          PMPrintingExpandedStateForPrint = lib.mkDefault true;
+          PMPrintingExpandedStateForPrint2 = lib.mkDefault true;
 
           # Save to disk (not to iCloud) by default
-          NSDocumentSaveNewDocumentsToCloud = false;
+          NSDocumentSaveNewDocumentsToCloud = lib.mkDefault false;
 
           # Display ASCII control characters using caret notation in standard text views
           # Try e.g. `cd /tmp; unidecode "\x{0000}" > cc.txt; open -e cc.txt`
-          NSTextShowsControlCharacters = true;
+          NSTextShowsControlCharacters = lib.mkDefault true;
 
           # Disable automatic termination of inactive apps
-          NSDisableAutomaticTermination = true;
+          NSDisableAutomaticTermination = lib.mkDefault true;
 
           # Override the keyboard repeat rate to something more sensible.
-          KeyRepeat = 2;
-          InitialKeyRepeat = 15;
+          KeyRepeat = lib.mkDefault 2;
+          InitialKeyRepeat = lib.mkDefault 15;
 
           # Disable automatic capitalization as it’s annoying when typing code
-          NSAutomaticCapitalizationEnabled = false;
+          NSAutomaticCapitalizationEnabled = lib.mkDefault false;
 
           # Disable smart dashes as they’re annoying when typing code
-          NSAutomaticDashSubstitutionEnabled = false;
+          NSAutomaticDashSubstitutionEnabled = lib.mkDefault false;
 
           # Disable automatic period substitution as it’s annoying when typing code
-          NSAutomaticPeriodSubstitutionEnabled = false;
+          NSAutomaticPeriodSubstitutionEnabled = lib.mkDefault false;
 
           # Disable smart quotes as they’re annoying when typing code
-          NSAutomaticQuoteSubstitutionEnabled = false;
+          NSAutomaticQuoteSubstitutionEnabled = lib.mkDefault false;
 
           # Disable auto-correct
-          NSAutomaticSpellingCorrectionEnabled = false;
+          NSAutomaticSpellingCorrectionEnabled = lib.mkDefault false;
 
           # Enable subpixel font rendering on non-Apple LCDs
           # Reference: https://github.com/kevinSuttle/macOS-Defaults/issues/17#issuecomment-266633501
-          AppleFontSmoothing = 1;
+          AppleFontSmoothing = lib.mkDefault 1;
 
           # Enable full keyboard access for all controls
           # (e.g. enable Tab in modal dialogs)
-          AppleKeyboardUIMode = 3;
+          AppleKeyboardUIMode = lib.mkDefault 3;
 
           # Disable press-and-hold for keys in favor of key repeat
-          ApplePressAndHoldEnabled = false;
+          ApplePressAndHoldEnabled = lib.mkDefault false;
 
           # Finder: show all filename extensions
-          AppleShowAllExtensions = true;
+          AppleShowAllExtensions = lib.mkDefault true;
 
           # Enable spring loading for directories
-          "com.apple.springing.enabled" = true;
+          "com.apple.springing.enabled" = lib.mkDefault true;
 
           # Remove the spring loading delay for directories
-          "com.apple.springing.delay" = 0.0;
+          "com.apple.springing.delay" = lib.mkDefault 0.0;
         };
 
-        screencapture.location = "${config.users.users.wnasreddine.home}/Pictures/Screenshots";
+        screencapture.location = lib.mkDefault "${config.users.users.wnasreddine.home}/Pictures/Screenshots";
 
         trackpad = {
           # Trackpad: enable tap to click
-          Clicking = true;
+          Clicking = lib.mkDefault true;
 
           # Trackpad: enable three-finger drag
-          TrackpadThreeFingerDrag = true;
+          TrackpadThreeFingerDrag = lib.mkDefault true;
 
           # Trackpad: enable right click on the bottom right of the trackpad
-          TrackpadRightClick = true;
+          TrackpadRightClick = lib.mkDefault true;
         };
 
         WindowManager = {
           # Hide all items from Desktop
-          StandardHideDesktopIcons = true;
+          StandardHideDesktopIcons = lib.mkDefault true;
         };
       };
 
       keyboard = {
         # Remap CapsLock to Control
-        enableKeyMapping = true;
-        remapCapsLockToControl = true;
+        enableKeyMapping = lib.mkDefault true;
+        remapCapsLockToControl = lib.mkDefault true;
       };
     };
   };
