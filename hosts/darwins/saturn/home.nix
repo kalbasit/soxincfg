a2a29a0fedf1e2325b17669871e4d5ae0a70f923
Wael Nasreddine <wael.nasreddine@gmail.com>
2026-02-17 20:43:58 -0800

    feat(saturn): Install nuShell

--- hosts/darwins/saturn/home.nix
+++ hosts/darwins/saturn/home.nix
@@ -2,6 +2,7 @@
 {
   config,
   lib,
+  pkgs,
   ...
 }:
 
@@ -17,6 +18,42 @@ in
   ];
 
   home.stateVersion = "24.11";
+
+  # Configure Nushell that can be used as a login shell.
+  home.shell.enableNushellIntegration = true;
+  programs.nushell = {
+    enable = true;
+
+    environmentVariables = config.home.sessionVariables;
+
+    extraEnv = ''
+      # Convert PATH from string to list
+      $env.PATH = ($env.PATH | split row (char esep))
+
+      # Add paths using std path add (prepends by default)
+      use std/util "path add"
+
+      # Standard UNIX paths (add first = lower priority)
+      path add "/usr/local/bin"
+
+      # Homebrew (Apple Silicon)
+      path add "/opt/homebrew/sbin"
+      path add "/opt/homebrew/bin"
+
+      # Nix paths (add last = higher priority)
+      path add "/nix/var/nix/profiles/default/bin"
+      path add "/run/current-system/sw/bin"
+      path add "/etc/profiles/per-user/wnasreddine/bin"
+      path add ($env.HOME | path join ".nix-profile" "bin")
+      path add ($env.HOME | path join ".local" "bin")
+    '';
+
+    shellAliases = {
+      g = "git";
+      k = "kubectl";
+      serve_this = "${pkgs.python3}/bin/python -m http.server"; # Use port 8000 by default
+    };
+  };
 
   sops = {
     age.keyFile = "${homePath}/Library/Application Support/sops/age/keys.txt";
