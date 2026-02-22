364107ca575a377a84cce2f13a3cf89b242ec93d
Wael Nasreddine <wael.nasreddine@gmail.com>
2026-02-22 14:25:39 -0800

    feat(saturn-nixos-vm): install graphite CLI

--- hosts/nixoses/saturn-nixos-vm/home.nix
+++ hosts/nixoses/saturn-nixos-vm/home.nix
@@ -1,11 +1,16 @@
 # home-manager configuration for user `yl`
 { soxincfg }:
 
+{ pkgs, ... }:
 {
   imports = [
     soxincfg.nixosModules.profiles.myself
     soxincfg.nixosModules.profiles.miniserver.qemu-vm-guest
     soxincfg.nixosModules.profiles.server
+  ];
+
+  home.packages = [
+    pkgs.graphite-cli
   ];
 
   programs.claude-code.enable = true;
