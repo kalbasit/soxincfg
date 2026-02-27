0d152190a307d4297159c25f5e1ed8914a21f8e4
Wael Nasreddine <wael.nasreddine@gmail.com>
2026-02-27 14:04:17 -0800

    fix: do not configure saturn-nixos-vm to build on itself

--- modules/settings/nix/distributed-builds/nixos.nix
+++ modules/settings/nix/distributed-builds/nixos.nix
@@ -14,30 +14,31 @@ in
   config = mkIf cfg.enable {
     nix = {
       distributedBuilds = true;
-      buildMachines = [
-        {
-          hostName = "saturn-nixos-vm.bigeye-bushi.ts.net";
-          maxJobs = 4;
-          sshKey = builtins.toString config.sops.secrets.ssh_key_saturn-nixos-vm.path;
-          sshUser = "builder";
-          system = "aarch64-linux";
-          supportedFeatures = [ "big-parallel" ];
-        }
-      ]
-      ++ optionals (config.networking.hostName != "hercules") [
-        {
-          hostName = "hercules.bigeye-bushi.ts.net";
-          maxJobs = 15;
-          sshKey = builtins.toString config.sops.secrets.ssh_key_hercules.path;
-          sshUser = "builder";
-          system = "x86_64-linux";
-          supportedFeatures = [
-            "big-parallel"
-            "kvm"
-            "nixos-test"
-          ];
-        }
-      ];
+      buildMachines =
+        (optionals (config.networking.hostName != "saturn-nixos-vm") [
+          {
+            hostName = "saturn-nixos-vm.bigeye-bushi.ts.net";
+            maxJobs = 4;
+            sshKey = builtins.toString config.sops.secrets.ssh_key_saturn-nixos-vm.path;
+            sshUser = "builder";
+            system = "aarch64-linux";
+            supportedFeatures = [ "big-parallel" ];
+          }
+        ])
+        ++ (optionals (config.networking.hostName != "hercules") [
+          {
+            hostName = "hercules.bigeye-bushi.ts.net";
+            maxJobs = 15;
+            sshKey = builtins.toString config.sops.secrets.ssh_key_hercules.path;
+            sshUser = "builder";
+            system = "x86_64-linux";
+            supportedFeatures = [
+              "big-parallel"
+              "kvm"
+              "nixos-test"
+            ];
+          }
+        ]);
     };
 
     sops.secrets = {
