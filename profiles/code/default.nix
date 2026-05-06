f6a2fa265fcbffa3e80945b8c222e64e428225d6
Wael Nasreddine <wael.nasreddine@gmail.com>
2026-05-05 19:49:07 -0700

    feat: install more stuff in the code profile

--- profiles/code/default.nix
+++ profiles/code/default.nix
@@ -11,14 +11,23 @@ in
 {
   imports = [
     soxincfg.nixosModules.profiles.myself
+    soxincfg.nixosModules.profiles.neovim.full
   ]
   ++ optionals (mode == "NixOS") [ ./nixos.nix ]
   ++ optionals (mode == "home-manager") [ ./home.nix ];
 
   config = {
     soxin = {
+      programs = {
+        less.enable = true;
+      };
+
       services = {
         openssh.enable = true;
+      };
+
+      virtualisation = {
+        docker.enable = true;
       };
     };
 
@@ -26,9 +35,12 @@ in
       programs = {
         claude-code.enable = true;
         codex.enable = true;
+        fzf.enable = true;
+        git.enable = true;
         pet.enable = true;
         ssh.enable = true;
         starship.enable = true;
+        t3code.enable = true;
         tmux.enable = true;
         zsh.enable = true;
       };
--- profiles/code/home.nix
+++ profiles/code/home.nix
@@ -1,7 +1,50 @@
-{ pkgs, ... }:
 {
+  config,
+  pkgs,
+  ...
+}:
+
+{
+  # programs
+  programs.bat.enable = true;
+  programs.direnv.enable = true;
+
+  # files
+  home.file = {
+    ".npmrc".text = "prefix=${config.home.homeDirectory}/.filesystem";
+  };
+
   home.packages = [
+    pkgs.amazon-ecr-credential-helper
+    pkgs.awscli2
+    pkgs.binutils # for strings
+    pkgs.bitwarden-cli
+    pkgs.colordiff
+    pkgs.dnsutils # for dig
+    pkgs.docker-credential-gcr
+    pkgs.file
     pkgs.flyctl
+    pkgs.gdb
+    pkgs.gh # GitHub command line client
+    pkgs.git-spice
+    pkgs.gnugrep
+    pkgs.gnupg
+    pkgs.hexyl # hex viewer with nice colors
+    pkgs.imagemagick # for convert
+    pkgs.inetutils # for telnet
+    pkgs.jq
+    pkgs.killall
+    pkgs.minio-client
+    pkgs.nil # Nix language server
+    pkgs.nix-index
+    pkgs.nix-zsh-completions
+    pkgs.nixd
+    pkgs.nixfmt
+    pkgs.skopeo # inspect docker images
     pkgs.turso-cli
+    pkgs.unzip
+    pkgs.watch
+    pkgs.wget
+    pkgs.yq-go
   ];
 }
