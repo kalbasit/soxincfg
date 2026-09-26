4f8284ec4be18a857d7fb55c9f687624b957df12
Wael Nasreddine <wael.nasreddine@gmail.com>
2026-09-25 21:15:31 -0700

    chore: format all files

--- profiles/workstation/nixos/local/nixos.nix
+++ profiles/workstation/nixos/local/nixos.nix
@@ -53,7 +53,7 @@ in
   };
 
   # TODO: fix this!
-  system.systemBuilderCommands = ''ln -sfn /home/yl/.surfingkeys.js $out/.surfingkeys.js'';
+  system.systemBuilderCommands = "ln -sfn /home/yl/.surfingkeys.js $out/.surfingkeys.js";
 
   # work around bug https://github.com/NixOS/nixpkgs/issues/180175
   systemd.services.NetworkManager-wait-online.enable = lib.mkForce false;
